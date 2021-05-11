d4572f0f7dc41e138e3fb858c99f57426d4ea6a0
uolter <walter.traspadini@pagopa.it>
2021-05-11 18:37:59 +0200

    virtual network, application insight, log Analytics

---
+++ src/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "2.56.0"
+  constraints = "2.56.0"
+  hashes = [
+    "h1:jup9JYEl6B1ihxgKJKPASmy4mof2apFB4IB1PEkwMIE=",
+  ]
+}
---
+++ src/variables.tf
@@ -0,0 +1,45 @@
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "prefix" {
+  type    = string
+  default = "cstar"
+}
+
+variable "env_short" {
+  type = string
+}
+
+
+# Network
+variable "cidr_vnet" {
+    type = list(string)
+}
+
+## Monitor
+variable "law_sku" {
+  type        = string
+  description = "Sku of the Log Analytics Workspace"
+  default     = "PerGB2018"
+}
+
+variable "law_retention_in_days" {
+  type        = number
+  description = "The workspace data retention in days"
+  default     = 30
+}
+
+variable "law_daily_quota_gb" {
+  type        = number
+  description = "The workspace daily quota for ingestion in GB."
+  default     = -1
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    CreatedBy = "Terraform"
+  }
+}
