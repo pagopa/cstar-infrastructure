40d3762c3021ceb23443277d089a240cab097add
pasqualedevita <>
2021-05-28 15:57:18 +0200

    add ingress

--- src/aks/apply.sh
+++ src/aks/apply.sh
@@ -5,6 +5,11 @@ WORKDIR="$BASHDIR"
 
 kubectl apply -f "${WORKDIR}/00-namespaces/common"
 
-
-
-
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+    --namespace ingress \
+    -f "${WORKDIR}/01-ingress/common/ingress.yaml" \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
