1f1529605ebd93874d23e3c33a93fc47fb67cad0
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-06-08 10:23:16 +0200

    Aks add secrets (#7)
    * Add aks secrets scaffolder
    
    * move .terraform to root module for required by validate
    
    * Update bpd_secrets.tf (#8)
    
    add comment on each key
    
    Co-authored-by: pasqualedevita <>
    Co-authored-by: dariopelliccioli-sia <62424365+dariopelliccioli-sia@users.noreply.github.com>

--- src/aks/locals.tf
+++ src/aks/locals.tf
@@ -1,4 +1,6 @@
 locals {
-  project              = format("%s-%s", var.prefix, var.env_short)
-  event_hub_connection = "${format("%s-evh-ns", local.project)}.servicebus.windows.net:${var.event_hub_port}"
+  project                  = format("%s-%s", var.prefix, var.env_short)
+  event_hub_connection     = "${format("%s-evh-ns", local.project)}.servicebus.windows.net:${var.event_hub_port}"
+  key_vault                = format("%s-kv", local.project)
+  key_vault_resource_group = format("%s-sec-rg", local.project)
 }
