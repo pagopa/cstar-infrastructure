97f1fde988bf8a40fa3964dd9bb6363cd2861daf
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-06-14 11:58:08 +0200

    Add to secrets query

--- src/aks/secrets.tf
+++ src/aks/secrets.tf
@@ -8,5 +8,18 @@ module "key_vault_secrets_query" {
     "db-administrator-login",
     "db-bpd-login",
     "db-bpd-password",
+    "evh-bpd-winner-outcome.award-winner-key",
+    "evh-bpd-citizen-trx.bpd-citizen-key",
+    "evh-bpd-citizen-trx.bpd-payment-instrument-key",
+    "evh-bpd-trx.bpd-citizen-key",
+    "evh-bpd-trx.bpd-payment-instrument-key",
+    "evh-bpd-trx.bpd-point-processor-key",
+    "evh-bpd-trx-cashback.bpd-point-processor-key",
+    "evh-bpd-trx-cashback.bpd-winning-transaction-key",
+    "evh-bpd-trx-error.bpd-payment-instrument-key",
+    "evh-bpd-trx-error.bpd-point-processor-key",
+    "evh-bpd-trx-error.bpd-transaction-error-manager-key",
+    "evh-rtd-trx.bpd-payment-instrument-key",
+    "evh-rtd-trx.rtd-csv-connector-key"
   ]
 }
