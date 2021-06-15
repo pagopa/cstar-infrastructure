b3a19abc679c169da66fbb270ee1dc210cf08528
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-06-15 09:13:48 +0200

    Update secrets

--- src/aks/rtd_secrets.tf
+++ src/aks/rtd_secrets.tf
@@ -21,13 +21,13 @@ resource "kubernetes_secret" "rtdtransactionmanager" {
 
   data = {
     #sasl jaas config string for topic fa-trx
-    KAFKA_INVTRX_SASL_JAAS_CONFIG = ""
+    KAFKA_INVTRX_SASL_JAAS_CONFIG = format(local.jaas_config_template, "fa-trx", "XXXX", module.key_vault_secrets_query.values["evh-bpd-winner-outcome-award-winner-key"].value)
     #sasl jaas config string for topic bpd-trx
-    KAFKA_POINTTRX_SASL_JAAS_CONFIG = ""
+    KAFKA_POINTTRX_SASL_JAAS_CONFIG = format(local.jaas_config_template, "bpd-trx", "bpd-point-processor", module.key_vault_secrets_query.values["evh-bpd-trx-bpd-point-processor-key"].value)
     #sasl jaas config string for topic rtd-trx-error
-    KAFKA_RTDTX_ERROR_SASL_JAAS_CONFIG = ""
+    KAFKA_RTDTX_ERROR_SASL_JAAS_CONFIG = format(local.jaas_config_template, "rtd-trx-error", "bpd-payment-instrument", module.key_vault_secrets_query.values["evh-bpd-trx-error-bpd-payment-instrument-key"].value)
     #sasl jaas config string for topic rtd-trx
-    KAFKA_RTDTX_SASL_JAAS_CONFIG = ""
+    KAFKA_RTDTX_SASL_JAAS_CONFIG = format(local.jaas_config_template, "rtd-trx", "bpd-payment-instrument", module.key_vault_secrets_query.values["evh-rtd-trx-bpd-payment-instrument-key"].value)
   }
 
   type = "Opaque"
@@ -41,28 +41,31 @@ resource "kubernetes_secret" "postgres-credentials" {
 
   data = {
     POSTGRES_AWARD_DB_NAME    = "bpd"
-    POSTGRES_AWARD_HOST       = ""
-    POSTGRES_AWARD_PASSWORD   = ""
+    POSTGRES_AWARD_HOST       = local.postgres_hostname
+    POSTGRES_AWARD_PASSWORD   = module.key_vault_secrets_query.values["db-bpd-password"].value
     POSTGRES_AWARD_SCHEMA     = "bpd_award_period"
-    POSTGRES_AWARD_USERNAME   = "BPD_USER"
+    POSTGRES_AWARD_USERNAME   = format("%s@%s", module.key_vault_secrets_query.values["db-bpd-login"].value, local.postgres_hostname)
     POSTGRES_BPD_DB_NAME      = "bpd"
-    POSTGRES_BPD_PASSWORD     = ""
+    POSTGRES_BPD_HOST         = local.postgres_hostname
+    POSTGRES_BPD_PASSWORD     = module.key_vault_secrets_query.values["db-bpd-password"].value
     POSTGRES_BPD_SCHEMA       = "bpd_payment_instrument"
-    POSTGRES_BPD_USERNAME     = "BPD_USER"
+    POSTGRES_BPD_USERNAME     = format("%s@%s", module.key_vault_secrets_query.values["db-bpd-login"].value, local.postgres_hostname)
     POSTGRES_CITIZEN_DB_NAME  = "bpd"
-    POSTGRES_CITIZEN_PASSWORD = ""
+    POSTGRES_CITIZEN_HOST     = local.postgres_hostname
+    POSTGRES_CITIZEN_PASSWORD = module.key_vault_secrets_query.values["db-bpd-password"].value
     POSTGRES_CITIZEN_SCHEMA   = "bpd_citizen"
-    POSTGRES_CITIZEN_USERNAME = "BPD_USER"
-    POSTGRES_DB_NAME          = "rtd"
+    POSTGRES_CITIZEN_USERNAME = format("%s@%s", module.key_vault_secrets_query.values["db-bpd-login"].value, local.postgres_hostname)
     POSTGRES_FA_DB_NAME       = "fa"
-    POSTGRES_FA_PASSWORD      = ""
+    POSTGRES_FA_HOST          = local.postgres_hostname
+    POSTGRES_FA_PASSWORD      = module.key_vault_secrets_query.values["db-fa-password"].value
     POSTGRES_FA_SCHEMA        = "fa_payment_instrument"
-    POSTGRES_FA_USERNAME      = "FA_USER"
-    POSTGRES_HOST             = ""
-    POSTGRES_RTD_HOST         = ""
-    POSTGRES_PASSWORD         = ""
+    POSTGRES_FA_USERNAME      = format("%s@%s", module.key_vault_secrets_query.values["db-fa-login"].value, local.postgres_hostname)
+    POSTGRES_DB_NAME          = "rtd"
+    POSTGRES_HOST             = local.postgres_hostname
+    POSTGRES_RTD_HOST         = local.postgres_hostname
+    POSTGRES_PASSWORD         = module.key_vault_secrets_query.values["db-rtd-password"].value
     POSTGRES_SCHEMA           = "rtd"
-    POSTGRES_USERNAME         = "RTD_USER"
+    POSTGRES_USERNAME         = format("%s@%s", module.key_vault_secrets_query.values["db-rtd-login"].value, local.postgres_hostname)
   }
 
   type = "Opaque"
