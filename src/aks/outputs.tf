1a261e0b22a7bf175dada494bf64c1f5a43e3fdc
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-06-30 14:35:18 +0200

    Add ca.crt

--- src/aks/outputs.tf
+++ src/aks/outputs.tf
@@ -1,4 +1,8 @@
-output "azure_devops_token" {
+output "azure_devops_sa_token" {
   value     = data.kubernetes_secret.azure_devops_secret.data["token"]
   sensitive = true
+}
+
+output "azure_devops_sa_cacrt" {
+  value     = data.kubernetes_secret.azure_devops_secret.data["ca.crt"]
 }
