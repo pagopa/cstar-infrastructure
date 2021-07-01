dbfa95e081b2f1b902613cd0baca1cce5f027850
pasqualedevita <>
2021-07-01 18:28:06 +0200

    Add rbac reader

---
+++ src/aks/rbac.tf
@@ -0,0 +1,34 @@
+data "azuread_group" "adgroup_externals" {
+  display_name = format("%s-adgroup-externals", local.project)
+}
+
+resource "kubernetes_cluster_role" "cluster_reader" {
+  metadata {
+    name = "cluster-reader"
+  }
+
+  rule {
+    api_groups = ["extensions", "apps", ""]
+    resources  = ["pods", "namespaces"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_role_binding" "reader_binding" {
+  for_each = toset(var.rbac_namespaces)
+
+  metadata {
+    name      = "reader-binding"
+    namespace = each.key
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-reader"
+  }
+  subject {
+    kind      = "Group"
+    name      = data.azuread_group.adgroup_externals.object_id
+    namespace = "kube-system"
+  }
+}
