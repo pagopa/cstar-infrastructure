a4602b537f92fbf79b0b6fb465c4cd360baadf7d
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-07-01 12:41:52 +0200

    Save sa azdo keyvault (#36)
    * Move azure_devops sa to kube-system namespace
    
    * Save azure devops sa secrets into keyvault
    
    Co-authored-by: pasqualedevita <>

--- src/aks/locals.tf
+++ src/aks/locals.tf
@@ -1,8 +1,9 @@
 locals {
   project                         = format("%s-%s", var.prefix, var.env_short)
   event_hub_connection            = "${format("%s-evh-ns", local.project)}.servicebus.windows.net:${var.event_hub_port}"
-  key_vault                       = format("%s-kv", local.project)
+  key_vault_name                  = format("%s-kv", local.project)
   key_vault_resource_group        = format("%s-sec-rg", local.project)
+  key_vault_id                    = "${data.azurerm_subscription.current.id}/resourceGroups/${local.key_vault_resource_group}/providers/Microsoft.KeyVault/vaults/${local.key_vault_name}"
   postgres_hostname               = "${format("%s-postgresql", local.project)}.postgres.database.azure.com"
   postgres_replica_hostname       = "${format("%s-postgresql-rep", local.project)}.postgres.database.azure.com"
   storage_account_name            = replace(format("%s-blobstorage", local.project), "-", "")
