69c5c9407bcc36afe02fb837ddf5027263344091
pasqualedevita <>
2021-07-13 17:34:01 +0200

    more strict cert_renew_policy

--- src/security.tf
+++ src/security.tf
@@ -95,14 +95,6 @@ resource "azurerm_key_vault_access_policy" "cert_renew_policy" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = var.devops_service_connection_object_id
 
-  key_permissions = [
-    "Get",
-    "List",
-    "Update",
-    "Create",
-    "Import",
-  ]
-
   secret_permissions = [
     "Get",
     "List",
@@ -115,7 +107,6 @@ resource "azurerm_key_vault_access_policy" "cert_renew_policy" {
     "Import",
   ]
 }
-
 
 resource "azurerm_user_assigned_identity" "appgateway" {
   resource_group_name = azurerm_resource_group.sec_rg.name
