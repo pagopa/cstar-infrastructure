9dc699a3358d461ed9717981763b3b2f8b01b95d
Giulio Garau <giulio.garau@gmail.com>
2021-07-27 17:43:57 +0200

    Update V3__bpd.post.fk.sql
    move triggers

--- src/psql/migrations/UAT-CSTAR/bpd/V3__bpd.post.fk.sql
+++ src/psql/migrations/UAT-CSTAR/bpd/V3__bpd.post.fk.sql
@@ -38,3 +38,36 @@ ALTER TABLE ONLY bpd_payment_instrument.bpd_payment_instrument_history
     ADD CONSTRAINT bpd_payment_instrument_history_fk FOREIGN KEY (hpan_s) REFERENCES bpd_payment_instrument.bpd_payment_instrument(hpan_s);
 
 
+--
+-- Name: bpd_payment_instrument insert_bpd_payment_instrument_history_trg; Type: TRIGGER; Schema: bpd_payment_instrument; Owner: ddsadmin
+--
+
+CREATE TRIGGER insert_bpd_payment_instrument_history_trg AFTER INSERT ON bpd_payment_instrument.bpd_payment_instrument FOR EACH ROW WHEN ((new.enabled_b = true)) EXECUTE PROCEDURE bpd_payment_instrument.insert_bpd_payment_instrument_history();
+
+
+--
+-- Name: bpd_payment_instrument insert_update_bpd_payment_instrument_history_trg; Type: TRIGGER; Schema: bpd_payment_instrument; Owner: ddsadmin
+--
+
+CREATE TRIGGER insert_update_bpd_payment_instrument_history_trg AFTER UPDATE OF enrollment_t ON bpd_payment_instrument.bpd_payment_instrument FOR EACH ROW WHEN (((new.enabled_b = true) AND (new.enabled_b <> old.enabled_b) AND (old.enrollment_t <> new.enrollment_t))) EXECUTE PROCEDURE bpd_payment_instrument.insert_bpd_payment_instrument_history();
+
+
+--
+-- Name: bpd_payment_instrument update_bpd_payment_instrument_history_trg; Type: TRIGGER; Schema: bpd_payment_instrument; Owner: ddsadmin
+--
+
+CREATE TRIGGER update_bpd_payment_instrument_history_trg AFTER UPDATE OF cancellation_t ON bpd_payment_instrument.bpd_payment_instrument FOR EACH ROW EXECUTE PROCEDURE bpd_payment_instrument.update_bpd_payment_instrument_history();
+
+
+--
+-- Name: bpd_winning_transaction insert_transfer_bpd_winning_transaction_into_new_trg; Type: TRIGGER; Schema: bpd_winning_transaction; Owner: ddsadmin
+--
+
+CREATE TRIGGER insert_transfer_bpd_winning_transaction_into_new_trg BEFORE INSERT ON bpd_winning_transaction.bpd_winning_transaction FOR EACH ROW EXECUTE PROCEDURE bpd_winning_transaction.insert_transfer_bpd_winning_transaction_into_new_trg_func();
+
+
+--
+-- Name: bpd_winning_transaction insert_transfer_bpd_winning_transaction_into_new_trg; Type: TRIGGER; Schema: bpd_winning_transaction_tmp; Owner: ddsadmin
+--
+
+CREATE TRIGGER insert_transfer_bpd_winning_transaction_into_new_trg BEFORE INSERT ON bpd_winning_transaction_tmp.bpd_winning_transaction FOR EACH ROW EXECUTE PROCEDURE bpd_winning_transaction.insert_transfer_bpd_winning_transaction_into_new_trg_func();
