d5fdb030155c141ee8d1ba9009b96dbb3311b854
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-07-30 12:08:52 +0200

    Update scripts (#109)
    Co-authored-by: pasqualedevita <>

--- migration-scripts/db/06-final_operations.sh
+++ migration-scripts/db/06-final_operations.sh
@@ -13,31 +13,7 @@
 
 ### psql dir
 
-cd ".bash .bash src/psql/"
+cd "../../src/psql/"
 
-bash flyway.sh info "${subscription}" fa
-bash flyway.sh migrate "${subscription}" fa -target=2
-
-# TODO Test if restoring bpd before rtd works
+bash flyway.sh migrate "${subscription}" bpd -target=3
 bash flyway.sh info "${subscription}" bpd
-bash flyway.sh migrate "${subscription}" bpd -target=2
-
-# TODO fails on REFRESH MATERIALIZED VIEW rtd_database.payment_instrument_hpans; for permission denied
-bash flyway.sh info "${subscription}" rtd
-bash flyway.sh migrate "${subscription}" rtd -target=2
-
-# this apply will fail
-bash terraform.sh apply "${subscription}" -target='postgresql_role.user["BPD_USER"]'
-
-bash terraform.sh import "${subscription}" 'postgresql_role.user["BPD_USER"]' BPD_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["RTD_USER"]' RTD_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["FA_USER"]' FA_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["BPD_AWARD_PERIOD_REMOTE_USER"]' BPD_AWARD_PERIOD_REMOTE_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["BPD_PAYMENT_INSTRUMENT_REMOTE_USER"]' BPD_PAYMENT_INSTRUMENT_REMOTE_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["BPD_WINNING_TRANSACTION_REMOTE_USER"]' BPD_WINNING_TRANSACTION_REMOTE_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["DASHBOARD_PAGOPA_USER"]' DASHBOARD_PAGOPA_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["FA_PAYMENT_INSTRUMENT_REMOTE_USER"]' FA_PAYMENT_INSTRUMENT_REMOTE_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["MONITORING_USER"]' MONITORING_USER
-bash terraform.sh import "${subscription}" 'postgresql_role.user["tkm_acquirer_manager"]' tkm_acquirer_manager
-# bash terraform.sh plan "${subscription}"
-# bash terraform.sh apply "${subscription}"
