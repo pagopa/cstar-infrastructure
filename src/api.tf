01fbb662cdcf9ea4b3857742b4cbc88f8e5363d1
uolter <walter.traspadini@pagopa.it>
2021-08-06 11:31:47 +0200

    dummy

--- src/api.tf
+++ src/api.tf
@@ -15,6 +15,7 @@ locals {
 
 module "apim" {
   source                  = "git::https://github.com/pagopa/azurerm.git//api_management?ref=v1.0.43"
+  
   subnet_id               = module.apim_snet.id
   location                = azurerm_resource_group.rg_api.location
   name                    = format("%s-apim", local.project)
