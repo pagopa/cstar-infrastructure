8a70111a47c03340468994d9f1348c1037cd811f
uolter <walter.traspadini@pagopa.it>
2021-09-02 21:01:34 +0200

    🩹 fix db script ssl required and role not in use

--- src/psql/migrations/PROD-CSTAR/postgres/V3__update_user_password.sql
+++ src/psql/migrations/PROD-CSTAR/postgres/V3__update_user_password.sql
@@ -10,4 +10,4 @@ ALTER USER "BPD_PAYMENT_INSTRUMENT_REMOTE_USER" WITH PASSWORD '${bpdPaymentInstr
 ALTER USER "BPD_AWARD_PERIOD_REMOTE_USER" WITH PASSWORD '${bpdAwardPeriodRemoteUserPassword}';
 ALTER USER "BPD_WINNING_TRANSACTION_REMOTE_USER" WITH PASSWORD '${bpdWinningTransactionRemoteUserPassword}';
 ALTER USER "DASHBOARD_PAGOPA_USER" WITH PASSWORD '${dashboardPagopaUserPassword}';
-ALTER USER "tkm_acquirer_manager" WITH PASSWORD '${tkmAcquirerManagerUserPassword}';
+-- ALTER USER "tkm_acquirer_manager" WITH PASSWORD '${tkmAcquirerManagerUserPassword}';
--- src/psql/migrations/PROD-CSTAR/rtd/V4__set_dblink_ssl_require.sql
+++ src/psql/migrations/PROD-CSTAR/rtd/V4__set_dblink_ssl_require.sql
@@ -1,5 +1,5 @@
 ALTER SERVER bpd_payment_instrument_remote
-	OPTIONS (set sslmode 'require');
+	OPTIONS (ADD sslmode 'require');
 
 ALTER SERVER fa_payment_instrument_remote
-	OPTIONS (set sslmode 'require');
+	OPTIONS (ADD sslmode 'require');
