a287042583c8c66787cb1148036c137d23c23a41
pasqualedevita <>
2021-09-20 11:55:21 +0200

    rename

--- src/default.tf
+++ src/default.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group" "roleassignment_default_rg" {
-  name     = "roleassignment-default-rg"
+resource "azurerm_resource_group" "default_roleassignment_rg" {
+  name     = "default-roleassignment-rg"
   location = var.location
 
   tags = var.tags
