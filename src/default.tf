0474af3e6dab535f94d4cbbc9793a786e65d1e11
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-09-20 12:02:59 +0200

    add comment

--- src/default.tf
+++ src/default.tf
@@ -1,4 +1,5 @@
 resource "azurerm_resource_group" "default_roleassignment_rg" {
+  #Important: do not create any resource inside this resource group
   name     = "default-roleassignment-rg"
   location = var.location
 
