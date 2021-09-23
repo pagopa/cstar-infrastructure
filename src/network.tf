93aa2b781db652d64e76e0a507630d91ca481e9a
uolter <walter.traspadini@pagopa.it>
2021-09-23 16:23:39 +0200

    renamed sercice principal for azure devops

--- src/network.tf
+++ src/network.tf
@@ -336,7 +336,7 @@ module "app_gw" {
 
   # Logs
   sec_log_analytics_workspace_id = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_workspace_id[0].value : null
-  sec_storage_id = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_storage_id[0].value : null
+  sec_storage_id                 = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_storage_id[0].value : null
 
   tags = var.tags
 }
