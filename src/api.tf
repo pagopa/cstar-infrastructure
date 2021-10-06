2c6100e6b9000e1d48ea91c79dd99f33457affdb
pasqualedevita <>
2021-10-06 16:18:59 +0200

    CEN-797] Align management dns record

--- src/api.tf
+++ src/api.tf
@@ -57,7 +57,7 @@ module "apim" {
   depends_on = [
     azurerm_application_insights.application_insights
   ]
-  
+
   # Logs
   sec_log_analytics_workspace_id = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_workspace_id[0].value : null
   sec_storage_id                 = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_storage_id[0].value : null
