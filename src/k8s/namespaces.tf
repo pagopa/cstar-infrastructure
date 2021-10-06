654872b152e4e943c8db2a679303a8d210065c5b
Giovanni <g.mancini.0@gmail.com>
2021-10-06 14:05:48 +0200

    [CEN-746] remove prometheus helm chart and namespace

--- src/k8s/namespaces.tf
+++ src/k8s/namespaces.tf
@@ -1,10 +1,3 @@
-resource "kubernetes_namespace" "monitoring" {
-  count = var.env_short == "d" ? 1 : 0 # only in dev
-  metadata {
-    name = "monitoring"
-  }
-}
-
 resource "kubernetes_namespace" "ingress" {
   metadata {
     name = "ingress"
