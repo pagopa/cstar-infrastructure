da91498a821af3e03481e0ece835990069c83ec1
pasqualedevita <>
2021-10-06 18:11:12 +0200

    fix-apim-logs

--- src/api.tf
+++ src/api.tf
@@ -21,7 +21,7 @@ locals {
 
 module "apim" {
 
-  source                  = "git::https://github.com/pagopa/azurerm.git//api_management?ref=v1.0.58"
+  source                  = "git::https://github.com/pagopa/azurerm.git//api_management?ref=v1.0.63"
   subnet_id               = module.apim_snet.id
   location                = azurerm_resource_group.rg_api.location
   name                    = format("%s-apim", local.project)
