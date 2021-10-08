288d3792a25019fe8cb1fa3edf78484a2ebdbe5a
Gianpietro.Cutolo <gianpietro.cutolo@pagopa.it>
2021-10-08 15:05:18 +0200

    env var for workspace fixed

--- src/main.tf
+++ src/main.tf
@@ -28,8 +28,7 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_monitor_diagnostic_setting" "ActivityLog" {
-
-  count                          = data.azurerm_key_vault_secret.sec_workspace_id[0].value != null ? 1 : 0
+  count                          = var.env_short == "p" ? 1 : 0
   name                           = "SecurityLogs"
   target_resource_id             = format("/subscriptions/%s", data.azurerm_key_vault_secret.sec_sub_id.value)
   log_analytics_workspace_id     = data.azurerm_key_vault_secret.sec_workspace_id[0].value
