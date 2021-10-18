69696d163d1846bfda7af774e46c7627f69f4d9d
giacut <88658486+giacut@users.noreply.github.com>
2021-10-18 12:11:01 +0200

    align terraform with ActivityLog conf, plus adding Alert logs (#223)
    * align terraform with ActivityLog conf, plus adding Alert logs
    
    * fixing syntax error
    
    * var workspace_id fixed
    
    * target_resource_id needs the full subscription path and not just the id
    
    * env var for workspace fixed
    
    * allign with naming convention
    
    * moving the sub activity logs to monitor
    
    * locals
    
    * linting
    
    * read the sub id from key vault module
    
    Co-authored-by: Gianpietro.Cutolo <gianpietro.cutolo@pagopa.it>

--- src/monitor.tf
+++ src/monitor.tf
@@ -63,3 +63,40 @@ resource "azurerm_monitor_action_group" "slack" {
   tags = var.tags
 }
 
+resource "azurerm_monitor_diagnostic_setting" "activity_log" {
+  count                          = var.env_short == "p" ? 1 : 0
+  name                           = "SecurityLogs"
+  target_resource_id             = module.key_vault.id 
+  log_analytics_workspace_id     = data.azurerm_key_vault_secret.sec_workspace_id[0].value
+  storage_account_id             = data.azurerm_key_vault_secret.sec_storage_id[0].value
+
+  log {
+
+    category = "Administrative"
+    enabled = true
+    retention_policy {
+      enabled = true
+      days = 365
+    }
+  }
+
+  log {
+
+    category  = "Security"
+    enabled = true
+    retention_policy {
+      enabled = true
+      days = 365
+    }
+  }
+
+  log {
+
+    category = "Alert"
+    enabled = true
+    retention_policy {
+      enabled = true
+      days = 365
+    }
+  }
+}
