61fc7397b7c859df750c5de220c5f7c3c50a1473
Gianpietro.Cutolo <gianpietro.cutolo@pagopa.it>
2021-10-18 11:25:15 +0200

    linting

--- src/main.tf
+++ src/main.tf
@@ -28,5 +28,5 @@ data "azurerm_subscription" "current" {}
 data "azurerm_client_config" "current" {}
 
 locals {
-￼  project = format("%s-%s", var.prefix, var.env_short)
-￼}
+  project = format("%s-%s", var.prefix, var.env_short)
+}
