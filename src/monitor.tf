854da8c775b6f7964b9683dedab12c6f86e31dc5
Gianpietro.Cutolo <gianpietro.cutolo@pagopa.it>
2021-10-19 10:31:16 +0200

    forward platform logs to storage account too

--- src/monitor.tf
+++ src/monitor.tf
@@ -64,29 +64,39 @@ resource "azurerm_monitor_action_group" "slack" {
 }
 
 resource "azurerm_monitor_diagnostic_setting" "activity_log" {
-  count                      = var.env_short == "p" ? 1 : 0
-  name                       = "SecurityLogs"
-  target_resource_id         = module.key_vault.id
-  log_analytics_workspace_id = data.azurerm_key_vault_secret.sec_workspace_id[0].value
-  storage_account_id         = data.azurerm_key_vault_secret.sec_storage_id[0].value
+  count                          = var.env_short == "p" ? 1 : 0
+  name                           = "SecurityLogs"
+  target_resource_id             = data.azurerm_subscription.current.subscription_id 
+  log_analytics_workspace_id     = data.azurerm_key_vault_secret.sec_workspace_id[0].value
+  storage_account_id             = data.azurerm_key_vault_secret.sec_storage_id[0].value
 
   log {
 
-    category = "AuditEvent"
-    enabled  = true
+    category = "Administrative"
+    enabled = true
     retention_policy {
       enabled = true
-      days    = 365
+      days = 365
     }
   }
 
   log {
 
-    category = "AzurePolicyEvaluationDetails"
-    enabled  = true
+    category  = "Security"
+    enabled = true
     retention_policy {
       enabled = true
-      days    = 365
+      days = 365
+    }
+  }
+
+  log {
+
+    category = "Alert"
+    enabled = true
+    retention_policy {
+      enabled = true
+      days = 365
     }
   }
 }
