c4f8a443cbc09a82420710f6de2d97e53d28bedc
Gianpietro.Cutolo <gianpietro.cutolo@pagopa.it>
2021-10-19 10:45:25 +0200

    pass the input vars to kv module

--- src/security.tf
+++ src/security.tf
@@ -14,6 +14,10 @@ module "key_vault" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
   lock_enable         = var.lock_enable
 
+  # Security Logs
+  sec_log_analytics_workspace_id = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_workspace_id[0].value : null
+  sec_storage_id                 = var.env_short == "p" ? data.azurerm_key_vault_secret.sec_storage_id[0].value : null
+
   tags = var.tags
 }
 
