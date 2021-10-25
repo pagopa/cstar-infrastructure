67d35ad74fa84e2e5f5621058d517f990a751309
Giovanni <g.mancini.0@gmail.com>
2021-10-25 09:15:44 +0200

    [CEN-821] Tune autoscale for citizen in dev

--- src/k8s/variables.tf
+++ src/k8s/variables.tf
@@ -192,3 +192,23 @@ variable "configmaps_fainvoiceprovider" {
 variable "configmaps_fatransactionerrormanager" {
   type = map(string)
 }
+
+variable "autoscaling_specs" {
+  type = map(object({
+    namespace = string
+    min_replicas = number
+    max_replicas = number
+    metrics = list(object({
+      type = string
+      resource = object({
+        name = string
+        target = object({
+          type = string
+          average_utilization = number
+        })
+      })
+    }))
+
+    }
+  ))
+}
