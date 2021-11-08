7ed1627efa76355d5d7e27987ef0a988b6ce7fd9
Giovanni <g.mancini.0@gmail.com>
2021-11-08 14:26:27 +0100

    [CEN-931] Simple clone (no indexes nor columns attributes) of bpd_citizen_ranking

---
+++ src/psql/migrations/PROD-CSTAR/bpd/V9__bpd_backup_citizen_ranking_for_supercashback.sql
@@ -0,0 +1,2 @@
+CREATE TABLE  bpd_citizen.bpd_citizen_ranking_before_supercashback_bkp SELECT * FROM bpd_citizen.bpd_citizen_ranking;
+GRANT SELECT ON TABLE bpd_citizen.bpd_citizen_ranking_before_supercashback_bkp TO "MONITORING_OPERATION_USER";
