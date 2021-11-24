661a9da3419ecf59966360b786ebc642438fcad3
Giovanni <g.mancini.0@gmail.com>
2021-11-24 13:22:48 +0100

    [CEN-1032] Fix variable aks_enable_auto_scaling and vaue of max_node_count

--- src/aks.tf
+++ src/aks.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg_aks" {
 }
 
 module "aks" {
-  source = "git::https://github.com/pagopa/azurerm.git//kubernetes_cluster?ref=v1.0.60"
+  source = "git::https://github.com/pagopa/azurerm.git//kubernetes_cluster?ref=CEN-1033-ignore-changes-on-aks-node-count"
 
   name                       = format("%s-aks", local.project)
   location                   = azurerm_resource_group.rg_aks.location
