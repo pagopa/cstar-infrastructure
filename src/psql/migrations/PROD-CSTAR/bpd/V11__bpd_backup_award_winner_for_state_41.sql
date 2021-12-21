ae37618ab7ecd964717366af3044fe7760c5c453
Giovanni <g.mancini.0@gmail.com>
2021-12-21 15:51:44 +0100

    [CEN-1080] Create a migration for bpd_award_winner backup

---
+++ src/psql/migrations/PROD-CSTAR/bpd/V11__bpd_backup_award_winner_for_state_41.sql
@@ -0,0 +1,2 @@
+CREATE TABLE  bpd_citizen.bpd_award_winner_bkp_state_41 AS SELECT * FROM bpd_citizen.bpd_award_winner;
+GRANT SELECT ON TABLE bpd_citizen.bpd_award_winner_bkp_state_41 TO "MONITORING_OPERATION_USER";
