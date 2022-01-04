7f279fcde201d4ed8d2ae189e48fa3c906cc4b09
Giovanni <g.mancini.0@gmail.com>
2022-01-04 11:57:11 +0100

    remove parameters of no longer existent VMs from setup script

--- src/psql/scripts/setup.sh
+++ src/psql/scripts/setup.sh
@@ -29,16 +29,6 @@ fi
 
 az account set -s "${SUBSCRIPTION}"
 
-vm_name=$(az vm list -d -o tsv --query "[?contains(name,'jumpbox')].{Name:name}")
-vm_resource_group_name=$(az vm list -d -o tsv --query "[?contains(name,'jumpbox')].{Name:resourceGroup}")
-vm_user_name=$(az vm list -d -o tsv --query "[?contains(name,'jumpbox')].{Name:osProfile.adminUsername}")
-vm_public_ip=$(az vm show -d -g "${vm_resource_group_name}" -n "${vm_name}" --query publicIps -o tsv)
-
-echo "vm_name=${vm_name}" > "${WORKDIR}/subscriptions/${SUBSCRIPTION}/.bastianhost.ini"
-echo "vm_resource_group_name=${vm_resource_group_name}" >> "${WORKDIR}/subscriptions/${SUBSCRIPTION}/.bastianhost.ini"
-echo "vm_user_name=${vm_user_name}" >> "${WORKDIR}/subscriptions/${SUBSCRIPTION}/.bastianhost.ini"
-echo "vm_public_ip=${vm_public_ip}" >> "${WORKDIR}/subscriptions/${SUBSCRIPTION}/.bastianhost.ini"
-
 psql_private_fqdn=$(az postgres server list -o tsv --query "[?contains(name,'postgresql')].{Name:fullyQualifiedDomainName}" | head -1)
 
 echo "psql_private_fqdn=${psql_private_fqdn}" >> "${WORKDIR}/subscriptions/${SUBSCRIPTION}/.bastianhost.ini"
