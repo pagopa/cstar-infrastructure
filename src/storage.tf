b22171acbb5079ee35b0e694d611d17e45429141
pasqualedevita <>
2022-01-04 10:40:04 +0100

    remove-aks-storage

--- src/storage.tf
+++ src/storage.tf
@@ -4,34 +4,6 @@ resource "azurerm_resource_group" "rg_storage" {
   tags     = var.tags
 }
 
-
-## Storage account to save aks terraform state
-module "aks_storage_account_terraform_state" {
-  source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=v1.0.5"
-
-  name            = replace(format("%s-saaksinfra", local.project), "-", "")
-  versioning_name = format("%s-sa-aksinfra-versioning", local.project)
-
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  access_tier              = "Hot"
-  enable_versioning        = true
-  resource_group_name      = azurerm_resource_group.rg_aks.name
-  location                 = var.location
-
-  tags = var.tags
-}
-
-# Container to stare the status file
-resource "azurerm_storage_container" "aks_state" {
-  depends_on = [module.aks_storage_account_terraform_state]
-
-  name                  = format("%s-aks-state", var.prefix)
-  storage_account_name  = module.aks_storage_account_terraform_state.name
-  container_access_type = "private"
-}
-
 ## Storage account to save psql terraform state
 module "psql_storage_account_terraform_state" {
   source = "git::https://github.com/pagopa/azurerm.git//storage_account?ref=v1.0.5"
