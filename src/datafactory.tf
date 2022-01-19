64b34b19edeeedab4396b91927de4a177e9397f4
Giovanni <g.mancini.0@gmail.com>
2022-01-19 19:30:44 +0100

    [CEN-1281] Fix parameters of tae_data_factory module to comply with the module defined in azurerm

--- src/datafactory.tf
+++ src/datafactory.tf
@@ -6,30 +6,34 @@ resource "azurerm_resource_group" "tae_df_rg" {
 
 module "tae_data_factory" {
   source = "git::https://github.com/pagopa/azurerm.git//data_factory?ref=CEN-1293-data-factory-module"
-  
+  #source = "git::https://github.com/pagopa/azurerm.git//data_factory?ref=v2.1.13"
+
+
   # Naming
-  location = var.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.tae_df_rg.name
-  name = "tae" # Transato per Agenzia delle Entrate
-  name_prefix = local.project
+  name                = format("%s-%s", local.project, "tae") # Transato per Agenzia delle Entrate
+  dns_a_record_name   = "tae"
 
   # Data Factory Repository
   github_conf = {
-    account_name    = "pagopa" # (Required) Specifies the GitHub account name.
-    branch_name     = "main" # - (Required) Specifies the collaboration branch of the repository to get code from.
+    account_name    = "pagopa"             # (Required) Specifies the GitHub account name.
+    branch_name     = "main"               # - (Required) Specifies the collaboration branch of the repository to get code from.
     git_url         = "https://github.com" # (Required) Specifies the GitHub Enterprise host name. For example: https://github.mydomain.com. Use https://github.com for open source repositories.
-    repository_name = "tae-data-factory" # (Required) Specifies the name of the git repository.
-    root_folder     = "/" #(Required) Specifies the root folder within the repository. Set to / for the top level.
+    repository_name = "tae-data-factory"   # (Required) Specifies the name of the git repository.
+    root_folder     = "/"                  #(Required) Specifies the root folder within the repository. Set to / for the top level.
   }
 
   # Networking & Security
 
-  subnet_id = module.adf_snet.id
-  private_dns_zone_rg_name = azurerm_resource_group.rg_vnet.name
-
-  private_dns_zone = {
-    id   = azurerm_private_dns_zone.adf.id
-    name = azurerm_private_dns_zone.adf.name
+  private_endpoint = {
+    enabled   = true
+    subnet_id = module.adf_snet.id
+    private_dns_zone = {
+      id   = azurerm_private_dns_zone.adf.id
+      name = azurerm_private_dns_zone.adf.name
+      rg   = azurerm_resource_group.rg_vnet.name
+    }
   }
 
   resources_managed_private_enpoint = tomap(
