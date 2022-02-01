2c2aa2b3f9d2700e3d663be8bdeb5c1a1992bc2d
Giovanni <g.mancini.0@gmail.com>
2022-02-01 18:13:02 +0100

    [CEN-1315] Avoid setting column (opt_in_status_s) default

--- src/psql/migrations/UAT-CSTAR/bpd/V22__bpd.alter_citizen_add_opt_in_status.sql
+++ src/psql/migrations/UAT-CSTAR/bpd/V22__bpd.alter_citizen_add_opt_in_status.sql
@@ -1,1 +1,2 @@
-ALTER TABLE bpd_citizen.bpd_citizen ADD COLUMN opt_in_status_n numeric DEFAULT 0;
+ALTER TABLE bpd_citizen.bpd_citizen ADD COLUMN opt_in_status_s VARCHAR(10);
+ALTER TABLE bpd_citizen.bpd_citizen ALTER COLUMN opt_in_status_s SET DEFAULT 'NOREQ';
