529c9e3f08459285c7d2c54bf51cbd15cb02baea
Giovanni <g.mancini.0@gmail.com>
2022-02-11 17:26:18 +0100

    [CEN-1361] modify resource group of storage topic

--- src/events.tf
+++ src/events.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "events_rg" {
 resource "azurerm_eventgrid_system_topic" "storage_topic" {
   name                   = format("%s-events-storage-topic", local.project)
   location               = var.location
-  resource_group_name    = azurerm_resource_group.events_rg.name
+  resource_group_name    = azurerm_resource_group.rg_storage.name
   source_arm_resource_id = module.cstarblobstorage.id
   topic_type             = "Microsoft.Storage.StorageAccounts"
 }
