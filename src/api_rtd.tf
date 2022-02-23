70d2316356362386fc334223245124fdcfe08f32
Giovanni <g.mancini.0@gmail.com>
2022-02-23 20:32:18 +0100

    [CEN-1362] refactor api and add random provider

---
+++ src/api_rtd.tf
@@ -0,0 +1,257 @@
+#
+# RTD PRODUCTS
+#
+
+module "rtd_api_product" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v1.0.42"
+
+  product_id   = "rtd-api-product"
+  display_name = "RTD_API_Product"
+  description  = "RTD_API_Product"
+
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  published             = true
+  subscription_required = true
+  approval_required     = true
+
+  subscriptions_limit = 50
+
+  policy_xml = file("./api_product/rtd_api/policy.xml")
+}
+
+module "rtd_api_product_internal" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v2.2.0"
+
+  product_id   = "rtd-api-product-internal"
+  display_name = "RTD_API_Product Internal"
+  description  = "RTD_API_Product Internal"
+
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  published             = true
+  subscription_required = true
+  approval_required     = true
+
+  subscriptions_limit = 5
+
+  policy_xml = templatefile("./api_product/rtd_api_internal/policy.xml.tpl", {
+    k8s-cluster-ip-range-from = var.k8s_ip_filter_range.from
+    k8s-cluster-ip-range-to   = var.k8s_ip_filter_range.to
+  })
+}
+
+#
+# RTD API
+#
+
+## azureblob ## 
+module "api_azureblob" {
+  source              = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
+  name                = format("%s-azureblob", var.env_short)
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  description  = ""
+  display_name = "azureblob"
+  path         = "pagopastorage"
+  protocols    = ["https"]
+
+  service_url = format("https://%s", azurerm_private_endpoint.blob_storage_pe.private_dns_zone_configs[0].record_sets[0].fqdn)
+
+  content_format = "openapi"
+  content_value = templatefile("./api/azureblob/openapi.json.tpl", {
+    host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
+  })
+
+  xml_content = file("./api/base_policy.xml")
+
+  product_ids           = var.enable.rtd.internal_api ? [module.rtd_api_product_internal.product_id] : [module.rtd_api_product.product_id]
+  subscription_required = true
+
+  api_operation_policies = []
+}
+
+## RTD Payment Instrument Manager API ##
+module "rtd_payment_instrument_manager" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v1.0.16"
+
+  name                = format("%s-rtd-payment-instrument-manager-api", var.env_short)
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+
+  description  = ""
+  display_name = "RTD Payment Instrument Manager API"
+  path         = "rtd/payment-instrument-manager"
+  protocols    = ["https", "http"]
+
+  service_url = format("http://%s/rtdmspaymentinstrumentmanager/rtd/payment-instrument-manager", var.reverse_proxy_ip)
+
+
+
+  content_value = templatefile("./api/rtd_payment_instrument_manager/swagger.xml.tpl", {
+    host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
+  })
+
+  xml_content = file("./api/base_policy.xml")
+
+  product_ids           = [module.rtd_api_product.product_id]
+  subscription_required = true
+
+  api_operation_policies = [
+    {
+      operation_id = "get-hash-salt",
+      xml_content = templatefile("./api/rtd_payment_instrument_manager/get-hash-salt_policy.xml.tpl", {
+        pm-backend-url                       = var.pm_backend_url,
+        rtd-pm-client-certificate-thumbprint = data.azurerm_key_vault_secret.rtd_pm_client-certificate-thumbprint.value
+        env_short                            = var.env_short
+      })
+    },
+    {
+      operation_id = "get-hashed-pans",
+      xml_content = templatefile("./api/rtd_payment_instrument_manager/get-hashed-pans_policy.xml.tpl", {
+        # as-is due an application error in prod -->  to-be
+        host = var.env_short == "p" ? "prod.cstar.pagopa.it" : trim(azurerm_dns_a_record.dns_a_appgw_api.fqdn, ".")
+      })
+    },
+  ]
+}
+
+## RTD CSV Transaction API ##
+module "rtd_csv_transaction" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.1.13"
+
+  count               = var.env_short == "p" ? 0 : 1
+  name                = format("%s-rtd-csv-transaction-api", var.env_short)
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  description  = "API providing upload methods for csv transaction files"
+  display_name = "RTD CSV Transaction API"
+  path         = "rtd/csv-transaction"
+  protocols    = ["https"]
+
+  service_url = format("https://%s", module.cstarblobstorage.primary_blob_host)
+
+  content_format = "openapi"
+  content_value = templatefile("./api/rtd_csv_transaction/openapi.json.tpl", {
+    host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
+  })
+
+  xml_content = file("./api/base_policy.xml")
+
+  product_ids           = [module.rtd_api_product.product_id]
+  subscription_required = true
+
+  api_operation_policies = [
+    {
+      operation_id = "createAdeSasToken",
+      xml_content = templatefile("./api/rtd_csv_transaction/create-sas-token-policy.xml.tpl", {
+        blob-storage-access-key       = module.cstarblobstorage.primary_access_key,
+        blob-storage-account-name     = module.cstarblobstorage.name,
+        blob-storage-container-prefix = "ade-transactions"
+      })
+    },
+    {
+      operation_id = "createRtdSasToken",
+      xml_content = templatefile("./api/rtd_csv_transaction/create-sas-token-policy.xml.tpl", {
+        blob-storage-access-key       = module.cstarblobstorage.primary_access_key,
+        blob-storage-account-name     = module.cstarblobstorage.name,
+        blob-storage-container-prefix = "rtd-transactions"
+      })
+    },
+    {
+      operation_id = "getPublicKey",
+      xml_content = templatefile("./api/rtd_csv_transaction/get-public-key-policy.xml.tpl", {
+        public-key-asc = data.azurerm_key_vault_secret.cstarblobstorage_public_key[0].value
+      })
+    },
+  ]
+}
+
+## RTD CSV Transaction Decrypted API ##
+module "rtd_csv_transaction_decrypted" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.2.0"
+
+  count               = var.env_short == "p" ? 0 : 1
+  name                = format("%s-rtd-csv-transaction-decrypted-api", var.env_short)
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  description  = "API providing upload methods for decrypted csv transaction files"
+  display_name = "RTD CSV Transaction Decrypted API"
+  path         = "rtd/csv-transaction-decrypted"
+  protocols    = ["https"]
+
+  service_url = format("https://%s", module.cstarblobstorage.primary_blob_host)
+
+  content_format = "openapi"
+  content_value = templatefile("./api/rtd_csv_transaction_decrypted/openapi.json.tpl", {
+    host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
+  })
+
+  xml_content = file("./api/base_policy.xml")
+
+  product_ids           = [module.rtd_api_product_internal.product_id]
+  subscription_required = true
+
+  api_operation_policies = [
+    {
+      operation_id = "createAdeSasToken",
+      xml_content = templatefile("./api/rtd_csv_transaction_decrypted/create-sas-token-policy.xml.tpl", {
+        blob-storage-access-key     = module.cstarblobstorage.primary_access_key,
+        blob-storage-account-name   = module.cstarblobstorage.name,
+        blob-storage-container-name = azurerm_storage_container.ade_transactions_decrypted.name
+      })
+    },
+    {
+      operation_id = "createRtdSasToken",
+      xml_content = templatefile("./api/rtd_csv_transaction_decrypted/create-sas-token-policy.xml.tpl", {
+        blob-storage-access-key     = module.cstarblobstorage.primary_access_key,
+        blob-storage-account-name   = module.cstarblobstorage.name,
+        blob-storage-container-name = azurerm_storage_container.rtd_transactions_decrypted.name
+      })
+    }
+  ]
+}
+
+# 
+# SUBSCRIPTIONS FOR INTERNAL USERS
+#
+resource "random_password" "rtd_internal_sub_key" {
+  count       = var.enable.rtd.internal_api ? 1 : 0
+  length      = 32
+  special     = false
+  upper       = false
+  min_numeric = 5
+  keepers = {
+    version = 1
+    date    = "2022-02-22"
+  }
+}
+
+resource "azurerm_api_management_subscription" "rtd_internal" {
+  count               = var.enable.rtd.internal_api ? 1 : 0
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+  product_id          = module.rtd_api_product_internal.id
+  display_name        = "Internal Microservices"
+  allow_tracing       = var.env_short == "d" ? true : false
+  primary_key         = random_password.rtd_internal_sub_key[count.index].result
+}
+
+resource "azurerm_key_vault_secret" "rtd_internal_api_product_subscription_key" {
+  count        = var.enable.rtd.internal_api ? 1 : 0
+  name         = "rtd-internal-api-product-subscription-key"
+  value        = random_password.rtd_internal_sub_key[count.index].result
+  content_type = "string"
+  key_vault_id = module.key_vault.id
+
+  depends_on = [
+    # create subscription, then store the key
+    azurerm_api_management_subscription.rtd_internal
+  ]
+}
