bffbea77ee6f529996231152e5042b05378b61fc
Alberto Re <alberto.re@pagopa.it>
2022-04-01 10:59:29 +0200

    [CEN-1469] Set auto_mitigate to false (as default)

--- src/monitor.tf
+++ src/monitor.tf
@@ -154,7 +154,6 @@ module "web_test_api" {
   ssl_cert_remaining_lifetime_check = 7
   expected_http_status              = each.value.expected_http_status
   content_validation                = "null"
-  auto_mitigate                     = true
 
   actions = [
     {
