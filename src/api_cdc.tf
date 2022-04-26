2411853e60bb0c358818be662262c9626a721c92
Giovanni <g.mancini.0@gmail.com>
2022-04-26 11:15:24 +0200

    [CDC-25] Expose health-check sogei (#369)
    * [CEN-25] Expose SOGEI Api via APIM
    
    * [CDC-25] Update module versions
    
    * [CDC-25] Reduce Sogei API to the bare minimum
    
    * [CDC-25] Refactor the cdc product policy to add the sogei api key
    
    * [CDC-25] Update module versions
    
    * [CDC-25] Add a dedicated resource group
    
    * [CDC-25] Modify resource group

--- src/api_cdc.tf
+++ src/api_cdc.tf
@@ -2,10 +2,32 @@
 # CDC PRODUCTS
 #
 
+data "azurerm_key_vault_secret" "cdc_sogei_api_key" {
+  name         = "x-ibm-client-secret-sogei-cdc"
+  key_vault_id = module.key_vault.id
+}
+
+resource "azurerm_api_management_named_value" "cdc_sogei_api_key" {
+  count = var.enable.cdc.api ? 1 : 0
+
+  name                = format("%s-x-ibm-client-secret", var.env_short)
+  resource_group_name = azurerm_resource_group.rg_api.name
+  api_management_name = module.apim.name
+
+  display_name = "x-ibm-client-secret"
+  secret       = true
+  value_from_key_vault {
+    secret_id = data.azurerm_key_vault_secret.cdc_sogei_api_key.id
+  }
+
+  # tags = merge(var.tags, { Application = "CDC" })
+
+}
+
 module "cdc_api_product" {
   count = var.enable.cdc.api ? 1 : 0
 
-  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v1.0.42"
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v2.12.5"
 
   product_id   = "cdc-api-product"
   display_name = "CDC_API_Product"
@@ -21,4 +43,41 @@ module "cdc_api_product" {
   subscriptions_limit = 50
 
   policy_xml = file("./api_product/cdc_api/policy.xml")
+
+  # tags = merge(var.tags, { Application = "CDC" })
+
+  depends_on = [
+    azurerm_api_management_named_value.cdc_sogei_api_key,
+  ]
+
+}
+
+module "api_cdc_sogei" {
+  count               = var.enable.cdc.api ? 1 : 0
+  source              = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.12.5"
+  name                = format("%s-cdc-sogei", var.env_short)
+  api_management_name = module.apim.name
+  resource_group_name = azurerm_resource_group.rg_api.name
+
+  description  = "API to request Carta della Cultura"
+  display_name = "Request Carta della Cultura"
+  path         = "sogei"
+  protocols    = ["https"]
+
+  service_url = "  https://apitest.sogei.it/interop/carta-cultura/"
+
+  content_format = "openapi"
+  content_value = templatefile("./api/cdc/openapi.sogei.yml.tpl", {
+    host = "https://apitest.sogei.it/interop/carta-cultura/"
+  })
+
+  xml_content = file("./api/base_policy.xml")
+
+  product_ids           = [module.cdc_api_product[count.index].product_id]
+  subscription_required = true
+
+  api_operation_policies = []
+
+  # tags = merge(var.tags, { Application = "CDC" })
+
 }
