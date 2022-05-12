89f1b8a8904c6b84b6f8ba22cbfb4b8587a0c8c7
Giovanni <g.mancini.0@gmail.com>
2022-05-12 21:01:19 +0200

    [CEN-1592] Upgrade k8s provider

--- src/k8s/main.tf
+++ src/k8s/main.tf
@@ -13,7 +13,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.3.2"
+      version = "~> 2.11.0"
     }
     helm = {
       source  = "hashicorp/helm"
