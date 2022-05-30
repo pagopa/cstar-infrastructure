6d2d8432f8e992c88edd183357a2aa1f711c0b4a
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-05-30 15:18:13 +0200

    aks: enable key vault addon

--- src/aks-platform/03_aks.tf
+++ src/aks-platform/03_aks.tf
@@ -71,7 +71,7 @@ module "aks" {
   aad_admin_group_ids = var.env_short == "d" ? [data.azuread_group.adgroup_admin.object_id, data.azuread_group.adgroup_developers.object_id, data.azuread_group.adgroup_externals.object_id] : [data.azuread_group.adgroup_admin.object_id]
 
   addon_azure_policy_enabled = var.aks_addons.azure_policy
-  # addon_azure_keyvault_secrets_provider_enabled = var.aks_addons.azure_key_vault_secrets_provider
+  addon_azure_key_vault_secrets_provider_enabled = var.aks_addons.azure_key_vault_secrets_provider
   addon_azure_pod_identity_enabled = var.aks_addons.pod_identity_enabled
 
   default_metric_alerts = var.aks_metric_alerts_default
