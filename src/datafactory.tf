2a05c4a3ef0979f0cdabb9ac43d668f59c8b6e3d
Giovanni <g.mancini.0@gmail.com>
2022-06-16 12:49:59 +0200

    [TAE-51] Restore module reference

--- src/datafactory.tf
+++ src/datafactory.tf
@@ -9,7 +9,7 @@ module "tae_data_factory" {
 
   count = var.enable.tae.adf ? 1 : 0
 
-  source = "git::https://github.com/pagopa/azurerm.git//data_factory?ref=TAE-51-set-system-managed-idenity-for-adf"
+  source = "git::https://github.com/pagopa/azurerm.git//data_factory?ref=v2.18.4"
 
 
   # Naming
