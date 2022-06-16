7ebbfef5473a613ee42a5a2e618e9394e02a3f14
Giovanni <g.mancini.0@gmail.com>
2022-06-16 22:27:24 +0200

    [TAE-53] Create ADF linked service for blob storage (#400)

--- src/datafactory.tf
+++ src/datafactory.tf
@@ -11,7 +11,6 @@ module "tae_data_factory" {
 
   source = "git::https://github.com/pagopa/azurerm.git//data_factory?ref=v2.18.4"
 
-
   # Naming
   location              = var.location
   resource_group_name   = azurerm_resource_group.tae_df_rg[count.index].name
@@ -48,4 +47,33 @@ module "tae_data_factory" {
   # Tags
   tags = var.tags
 
+}
+
+data "azurerm_data_factory" "tae_adf" {
+
+  count = var.enable.tae.adf ? 1 : 0
+
+  name                = format("%s-%s", local.project, "tae")
+  resource_group_name = azurerm_resource_group.tae_df_rg[count.index].name
+}
+
+data "azurerm_storage_account" "acquirer_sa" {
+
+  count = var.enable.tae.adf ? 1 : 0
+
+  name                = replace(format("%s-blobstorage", local.project), "-", "")
+  resource_group_name = format("%s-storage-rg", local.project)
+}
+
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "tae_adf_sa_linked_service" {
+
+  count = var.enable.tae.adf ? 1 : 0
+
+  resource_group_name = azurerm_resource_group.tae_df_rg[count.index].name
+  name                = format("%s-%s-sa-linked-service", local.project, "tae")
+  data_factory_id     = data.azurerm_data_factory.tae_adf[count.index].id
+
+  service_endpoint = data.azurerm_storage_account.acquirer_sa[count.index].primary_blob_endpoint
+
+  use_managed_identity = true
 }
