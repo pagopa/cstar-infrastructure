a245d273cd4c4842524586cd4504886dcfb57fc8
Giovanni <g.mancini.0@gmail.com>
2022-06-22 01:15:38 +0200

    [TAE-55] Run precommit check

--- src/README.md
+++ src/README.md
@@ -132,6 +132,8 @@
 | [azurerm_cosmosdb_mongo_database.transaction_aggregate](https://registry.terraform.io/providers/hashicorp/azurerm/2.99.0/docs/resources/cosmosdb_mongo_database) | resource |
 | [azurerm_data_factory_linked_service_azure_blob_storage.tae_adf_sa_linked_service](https://registry.terraform.io/providers/hashicorp/azurerm/2.99.0/docs/resources/data_factory_linked_service_azure_blob_storage) | resource |
 | [azurerm_data_factory_linked_service_cosmosdb_mongoapi.tae_adf_mongo_linked_service](https://registry.terraform.io/providers/hashicorp/azurerm/2.99.0/docs/resources/data_factory_linked_service_cosmosdb_mongoapi) | resource |
+| [azurerm_data_factory_pipeline.aggregates_ingestor](https://registry.terraform.io/providers/hashicorp/azurerm/2.99.0/docs/resources/data_factory_pipeline) | resource |
+| [azurerm_data_factory_trigger_blob_event.acquirer_aggregate](https://registry.terraform.io/providers/hashicorp/azurerm/2.99.0/docs/resources/data_factory_trigger_blob_event) | resource |
 | [azurerm_dns_a_record.dns-a-developer-production-cstar](https://registry.terraform.io/providers/hashicorp/azurerm/2.99.0/docs/resources/dns_a_record) | resource |
 | [azurerm_dns_a_record.dns-a-developer-test-cstar](https://registry.terraform.io/providers/hashicorp/azurerm/2.99.0/docs/resources/dns_a_record) | resource |
 | [azurerm_dns_a_record.dns-a-management-test-cstar](https://registry.terraform.io/providers/hashicorp/azurerm/2.99.0/docs/resources/dns_a_record) | resource |
--- src/tae_pipeline.tf
+++ src/tae_pipeline.tf
@@ -1,9 +1,9 @@
-resource "azurerm_data_factory_pipeline" "test" {
-  
-  count = var.enable.tae.adf ? 1 : 0
+resource "azurerm_data_factory_pipeline" "aggregates_ingestor" {
+
+  count               = var.enable.tae.adf ? 1 : 0
   resource_group_name = azurerm_resource_group.tae_df_rg[count.index].name
 
-  name            = "test"
+  name            = "aggregates_ingestor"
   data_factory_id = data.azurerm_data_factory.tae_adf[count.index].id
   variables = {
     "bob" = "item1"
@@ -28,20 +28,20 @@ resource "azurerm_data_factory_trigger_blob_event" "acquirer_aggregate" {
 
   count = var.enable.tae.adf ? 1 : 0
 
-  name                = format("%s-tae-acquirer-aggregates-trigger", local.project)
-  data_factory_id     = data.azurerm_data_factory.tae_adf[count.index].id
-  storage_account_id  = module.cstarblobstorage.id
-  events              = ["Microsoft.Storage.BlobCreated"]
-  blob_path_ends_with = ".decrypted"
+  name                  = format("%s-tae-acquirer-aggregates-trigger", local.project)
+  data_factory_id       = data.azurerm_data_factory.tae_adf[count.index].id
+  storage_account_id    = module.cstarblobstorage.id
+  events                = ["Microsoft.Storage.BlobCreated"]
+  blob_path_ends_with   = ".decrypted"
   blob_path_begins_with = "/ade-transactions-decrypted/"
-  ignore_empty_blobs  = true
-  activated           = true
+  ignore_empty_blobs    = true
+  activated             = true
 
   annotations = ["AcquirerAggregates"]
   description = "The trigger fires when an acquirer send aggregates files"
 
   pipeline {
-    name = azurerm_data_factory_pipeline.test[count.index].name
+    name = azurerm_data_factory_pipeline.aggregates_ingestor[count.index].name
     # parameters = {
     #   Env = "Prod"
     # }
