9442b53cd49fadb2403c74ed4def5e1797d299ef
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-06-30 17:49:01 +0200

    [RTD-219] filter files created in ade container for sftp subscription

--- src/sftp.tf
+++ src/sftp.tf
@@ -65,6 +65,9 @@ resource "azurerm_eventgrid_system_topic_event_subscription" "sftp" {
   system_topic         = azurerm_eventgrid_system_topic.sftp.name
   resource_group_name  = azurerm_resource_group.sftp.name
   eventhub_endpoint_id = data.azurerm_eventhub.rtd_platform_eventhub.id
+  subject_filter {
+    subject_begins_with = "/blobServices/default/containers/ade/blobs/"
+  }
 }
 
 resource "azurerm_storage_container" "ade" {
