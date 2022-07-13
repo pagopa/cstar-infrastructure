bce95570ffd2886c6e51fef535ecbc7798180254
dpellicc <dpellicc@everis.com>
2022-07-13 09:11:00 +0200

    IPE-126 add portal product and api

--- src/domains/idpay-app/04_apim_api_portal.tf
+++ src/domains/idpay-app/04_apim_api_portal.tf
@@ -1,32 +1,29 @@
 #
 # IDPAY PRODUCTS
 #
-/*
-module "idpay_api_io_product" {
+
+module "idpay_api_portal_product" {
   source = "git::https://github.com/pagopa/azurerm.git//api_management_product?ref=v2.18.2"
 
-  product_id   = "idpay_api_io_product"
-  display_name = "IDPAY_APP_IO_PRODUCT"
-  description  = "IDPAY_APP_IO_PRODUCT"
+  product_id   = "idpay_api_portal_product"
+  display_name = "IDPAY_APP_PORTAL_PRODUCT"
+  description  = "IDPAY_APP_PORTAL_PRODUCT"
 
   api_management_name = data.azurerm_api_management.apim_core.name
   resource_group_name = data.azurerm_resource_group.apim_rg.name
 
-  published             = true
-  subscription_required = true
-  approval_required     = true
+  published             = false
+  subscription_required = false
+  approval_required     = false
 
   subscriptions_limit = 50
 
-  policy_xml = templatefile("./api_product/policy.xml.tpl", {
-    env_short           = var.env_short
-    reverse_proxy_be_io = var.reverse_proxy_be_io
-    appio_timeout_sec   = var.appio_timeout_sec
+  policy_xml = templatefile("./api_product/portal_api/policy_portal.xml.tpl", {
+    jwt_cert_signing_kv_id = azurerm_api_management_certificate.idpay_token_exchange_cert_jwt.name
   })
 
-  groups = ["developers"]
 }
-*/
+
 #
 # IDPAY API
 #
@@ -51,8 +48,8 @@ module "idpay_permission_portal" {
 
   xml_content = file("./api/base_policy.xml")
 
-  #product_ids           = [module.idpay_api_io_product.product_id]
-  #subscription_required = true
+  product_ids           = [module.idpay_api_portal_product.product_id]
+  subscription_required = false
 
   api_operation_policies = [
     {
@@ -63,5 +60,40 @@ module "idpay_permission_portal" {
       })
     }
   ]
+
+}
+
+## IDPAY Welfare Portal Initiative API ##
+module "idpay_initiative_portal" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.18.2"
+
+  name                = "${var.env_short}-idpay-initiative"
+  api_management_name = data.azurerm_api_management.apim_core.name
+  resource_group_name = data.azurerm_resource_group.apim_rg.name
+
+  description  = "IDPAY Welfare Portal Initiative"
+  display_name = "IDPAY Welfare Portal Initiative API"
+  path         = "idpay/initiative"
+  protocols    = ["https", "http"]
+
+  service_url = "http://${var.ingress_load_balancer_hostname}/idpayportalwelfarebackeninitiative/idpay/initiative"
+  
+  content_format = "openapi"
+  content_value = file("./api/idpay_initiative/swagger.initiative.yml")
+
+  xml_content = file("./api/base_policy.xml")
+
+  product_ids           = [module.idpay_api_portal_product.product_id]
+  subscription_required = false
+
+/*  api_operation_policies = [
+    {
+      operation_id = "userPermission"
+      xml_content = templatefile("./api/idpay_role_permission/get-permission-policy.xml.tpl", {
+        ingress_load_balancer_hostname = var.ingress_load_balancer_hostname
+        jwt_cert_signing_kv_id = azurerm_api_management_certificate.idpay_token_exchange_cert_jwt.name
+      })
+    }
+  ]*/
 
 }
