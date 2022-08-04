fbe320b124e1257bc74f099482424cf93171609a
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-08-04 20:46:34 +0200

    KV: improved access policy with mangedContacts

---
+++ src/key_vault_access_policy.tf
@@ -0,0 +1,144 @@
+#
+# azure devops policy
+#
+
+#pagopaspa-cstar-platform-iac-projects-{subscription}
+data "azuread_service_principal" "platform_iac_sp" {
+  count        = var.enable_iac_pipeline ? 1 : 0
+  display_name = "pagopaspa-cstar-platform-iac-projects-${data.azurerm_subscription.current.subscription_id}"
+}
+
+resource "azurerm_key_vault_access_policy" "azdevops_platform_iac_policy" {
+  count        = var.enable_iac_pipeline ? 1 : 0
+  key_vault_id = module.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_service_principal.platform_iac_sp[0].object_id
+
+  secret_permissions = ["Get", "List", "Set", ]
+  storage_permissions = []
+  certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get", "ManageContacts",]
+}
+
+#
+# Azure Groups
+#
+
+data "azuread_group" "adgroup_admin" {
+  display_name = "${local.project}-adgroup-admin"
+}
+
+## ad group policy ##
+resource "azurerm_key_vault_access_policy" "ad_group_policy" {
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_admin.object_id
+
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  secret_permissions      = ["Get", "List", "Set", "Delete", ]
+  storage_permissions     = []
+  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover", "ManageContacts",]
+}
+
+data "azuread_group" "adgroup_developers" {
+  display_name = "${local.project}-adgroup-developers"
+}
+
+## ad group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_developers_policy" {
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_developers.object_id
+
+  key_permissions         = var.env_short == "d" ? ["Get", "List", "Update", "Create", "Import", "Delete", ] : ["Get", "List", "Update", "Create", "Import", ]
+  secret_permissions      = var.env_short == "d" ? ["Get", "List", "Set", "Delete", ] : ["Get", "List", "Set", ]
+  storage_permissions     = []
+  certificate_permissions = var.env_short == "d" ? ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover", "ManageContacts",] : ["Get", "List", "Update", "Create", "Import", "Restore", "Recover", "ManageContacts", ]
+}
+
+data "azuread_group" "adgroup_externals" {
+  display_name = "${local.project}-adgroup-externals"
+}
+
+## ad group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_externals_policy" {
+  count = var.env_short == "d" ? 1 : 0
+
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_externals.object_id
+
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  secret_permissions      = ["Get", "List", "Set", "Delete", ]
+  storage_permissions     = []
+  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover", "ManageContacts",]
+}
+
+data "azuread_group" "adgroup_security" {
+  display_name = "${local.project}-adgroup-security"
+}
+
+## ad group policy ##
+resource "azurerm_key_vault_access_policy" "adgroup_security_policy" {
+  count = var.env_short == "d" ? 1 : 0
+
+  key_vault_id = module.key_vault.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azuread_group.adgroup_security.object_id
+
+  key_permissions         = ["Get", "List", "Update", "Create", "Import", "Delete", ]
+  secret_permissions      = ["Get", "List", "Set", "Delete", ]
+  storage_permissions     = []
+  certificate_permissions = ["Get", "List", "Update", "Create", "Import", "Delete", "Restore", "Purge", "Recover", "ManageContacts",]
+}
+
+## azure devops ##
+data "azuread_service_principal" "azdo_sp_tls_cert" {
+  count        = var.azdo_sp_tls_cert_enabled ? 1 : 0
+  display_name = "azdo-sp-${local.project}-tls-cert"
+}
+
+resource "azurerm_key_vault_access_policy" "azdo_sp_tls_cert" {
+  count        = var.azdo_sp_tls_cert_enabled ? 1 : 0
+  key_vault_id = module.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_service_principal.azdo_sp_tls_cert[0].object_id
+
+  certificate_permissions = ["Get", "List", "Import", "ManageContacts",]
+}
+
+resource "azurerm_key_vault_access_policy" "cert_renew_policy" {
+  count        = var.devops_service_connection_object_id == null ? 0 : 1
+  key_vault_id = module.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = var.devops_service_connection_object_id
+
+  secret_permissions      = ["Get", "List", "Set", ]
+  certificate_permissions = ["Get", "List", "Import", "ManageContacts",]
+}
+
+## api management policy ##
+resource "azurerm_key_vault_access_policy" "api_management_policy" {
+  key_vault_id = module.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = module.apim.principal_id
+
+  key_permissions         = []
+  secret_permissions      = ["Get", "List"]
+  certificate_permissions = ["Get", "List", "ManageContacts",]
+  storage_permissions     = []
+}
+
+## user assined identity: (application gateway) ##
+resource "azurerm_key_vault_access_policy" "app_gateway_policy" {
+  key_vault_id            = module.key_vault.id
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = azurerm_user_assigned_identity.appgateway.principal_id
+  key_permissions         = ["Get", "List"]
+  secret_permissions      = ["Get", "List"]
+  certificate_permissions = ["Get", "List", "Purge", "ManageContacts",]
+  storage_permissions     = []
+}
