c9cc29b3fb8e9619252749a46cb579e7d01556a9
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-08-11 13:13:55 +0200

    added namespace to rbac permissions

--- src/aks-platform/04_rbac.tf
+++ src/aks-platform/04_rbac.tf
@@ -5,7 +5,7 @@ resource "kubernetes_cluster_role" "cluster_deployer" {
 
   rule {
     api_groups = [""]
-    resources  = ["services", "configmaps", "secrets"]
+    resources  = ["namespaces", "services", "configmaps", "secrets"]
     verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
   }
 
