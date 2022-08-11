33faa3bf5e1ff9c92212eab40fa7e27c4d28818d
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-08-11 11:02:14 +0200

    minor fix

--- src/domains/idpay-common/01_keyvault_0.tf
+++ src/domains/idpay-common/01_keyvault_0.tf
@@ -71,8 +71,7 @@ resource "azurerm_key_vault_access_policy" "azdevops_platform_iac_policy" {
 
   secret_permissions = ["Get", "List", "Set", ]
 
-  certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get"
-  ]
+  certificate_permissions = ["SetIssuers", "DeleteIssuers", "Purge", "List", "Get", ]
 
   storage_permissions = []
 }
