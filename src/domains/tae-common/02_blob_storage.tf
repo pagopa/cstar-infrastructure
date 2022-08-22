acad5dfdd883958f49109d75bffab2c23fb550b4
Giovanni <g.mancini.0@gmail.com>
2022-08-22 09:19:47 +0200

    [TAE-167] Add diagnostics log to acquirer blob storage

--- src/domains/tae-common/02_blob_storage.tf
+++ src/domains/tae-common/02_blob_storage.tf
@@ -7,3 +7,56 @@ data "azurerm_storage_account" "sftp_sa" {
   name                = replace("${local.product}-sftp", "-", "")
   resource_group_name = "${local.product}-sftp-rg"
 }
+
+
+#
+# Diagnostic settings
+#
+resource "azurerm_monitor_diagnostic_setting" "log_acquirer_sa" {
+  count                          = true ? 1 : 0
+  name                           = "LogAcquirerBlob"
+  target_resource_id             = format("%s/blobServices/default", data.azurerm_storage_account.acquirer_sa.id)
+  log_analytics_workspace_id     = data.azurerm_log_analytics_workspace.log_analytics.id
+  # storage_account_id             = var.sec_storage_id
+  log_analytics_destination_type = "Dedicated"
+
+  log {
+    category = "StorageRead"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+
+  log {
+    category = "StorageWrite"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+
+  log {
+    category = "StorageDelete"
+    enabled  = true
+
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+
+  metric {
+    category = "Transaction"
+    enabled  = false
+
+    retention_policy {
+      days    = 0
+      enabled = false
+    }
+  }
+}
