2c5039004468d023e12aa64e7abc1981656e4ddc
dpellicc <dpellicc@everis.com>
2022-08-24 23:02:36 +0200

    IREER-130 - add auth rule and keyvault secret

--- src/domains/idpay-common/04_service_bus.tf
+++ src/domains/idpay-common/04_service_bus.tf
@@ -7,6 +7,7 @@ resource "azurerm_servicebus_namespace" "idpay-service-bus-ns" {
   tags = var.tags
 }
 
+#QUEUE
 resource "azurerm_servicebus_queue" "idpay-onboarding-request" {
   name                                    = "idpay-onboarding-request"
   namespace_id                            = azurerm_servicebus_namespace.idpay-service-bus-ns.id
@@ -14,4 +15,43 @@ resource "azurerm_servicebus_queue" "idpay-onboarding-request" {
   duplicate_detection_history_time_window = "P1D"
   dead_lettering_on_message_expiration    = true
   #  enable_partitioning = true # default false
+}
+
+resource "azurerm_servicebus_queue_authorization_rule" "idpay-onboarding-request-producer" {
+  name     = "idpay-onboarding-request-producer"
+  queue_id = azurerm_servicebus_queue.idpay-onboarding-request.id
+
+  listen = false
+  send   = true
+  manage = false
+}
+
+#processor have to read and send bck the data with a delay if there is the need to reprocess
+resource "azurerm_servicebus_queue_authorization_rule" "idpay-onboarding-request-processor" {
+  name     = "idpay-onboarding-request-processor"
+  queue_id = azurerm_servicebus_queue.idpay-onboarding-request.id
+
+  listen = true
+  send   = true
+  manage = false
+}
+
+#KEY-VAULT Secret
+
+resource "azurerm_key_vault_secret" "idpay-onboarding-request-producer-sas-key" {
+
+  name         = "idpay-onboarding-request-producer-sas-key"
+  value        = azurerm_servicebus_queue_authorization_rule.idpay-onboarding-request-producer.primary_connection_string
+  content_type = "text/plain"
+
+  key_vault_id = module.key_vault_idpay.id
+}
+
+resource "azurerm_key_vault_secret" "idpay-onboarding-request-processor-sas-key" {
+
+  name         = "idpay-onboarding-request-processor-sas-key"
+  value        = azurerm_servicebus_queue_authorization_rule.idpay-onboarding-request-processor.primary_connection_string
+  content_type = "text/plain"
+
+  key_vault_id = module.key_vault_idpay.id
 }
