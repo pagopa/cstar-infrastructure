508ea321a8949f3a9e5343b80826da4c6786eb1a
andrea.petreti <andrea.petreti@pagopa.it>
2022-09-15 11:01:57 +0200

    removed todo from migrate-mongo config

--- src/mongo/migrations/DEV-CSTAR/migrate-mongo-config.js
+++ src/mongo/migrations/DEV-CSTAR/migrate-mongo-config.js
@@ -3,14 +3,10 @@
 
 const config = {
   mongodb: {
-    // TODO Change (or review) the url to your MongoDB:
     url: process.env.MONGO_URI,
-
-    // TODO Change this to your database name:
     databaseName: process.env.MONGO_DATABASE,
-
     options: {
-      useNewUrlParser: true, // removes a deprecation warning when connecting
+      useNewUrlParser: true, // reßmoves a deprecation warning when connecting
       useUnifiedTopology: true, // removes a deprecating warning when connecting
       //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
       //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
