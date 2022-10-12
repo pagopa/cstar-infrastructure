f95ede4a0209996c9290667056d74dd8da2c4402
Giovanni <g.mancini.0@gmail.com>
2022-10-12 13:06:32 +0200

    [IPE-419] rn precommit checks

--- src/domains/idpay-app/08_k8s_configmaps.tf
+++ src/domains/idpay-app/08_k8s_configmaps.tf
@@ -71,7 +71,7 @@ resource "kubernetes_config_map" "rest-client" {
     rest_client_schema            = "http"
     idpay_onboarding_host         = "http://idpay-onboarding-workflow-microservice-chart:8080"
     idpay_payment_instrument_host = "http://idpay-payment-instrument-microservice-chart:8080"
-    idpay_group_host              =  "http://idpay-group-microservice-chart:8080"
+    idpay_group_host              = "http://idpay-group-microservice-chart:8080"
     initiative_ms_base_url        = "http://idpay-portal-welfare-backend-initiative-microservice-chart:8080"
     checkiban_base_url            = var.checkiban_base_url
     checkiban_url                 = "/api/pagopa/banking/v4.0/utils/validate-account-holder"
