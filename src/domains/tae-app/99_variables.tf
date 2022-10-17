31b04545d65aea3a4e39ee48032e5f079e910299
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-10-17 11:06:40 +0200

    [RTD-716] add overnight bulk delete trigger config

--- src/domains/tae-app/99_variables.tf
+++ src/domains/tae-app/99_variables.tf
@@ -150,6 +150,23 @@ variable "ack_ingestor_conf" {
   }
 }
 
+variable "bulk_delete_aggregates_conf" {
+  type = object({
+    interval  = number
+    frequency = string
+    enable    = bool
+    hours     = number
+    minutes   = number
+  })
+  default = {
+    interval  = 1
+    frequency = "Day"
+    enable    = false
+    hours     = 3
+    minutes   = 0
+  }
+}
+
 variable "dexp_tae_db_linkes_service" {
   type = object({
     enable = bool
