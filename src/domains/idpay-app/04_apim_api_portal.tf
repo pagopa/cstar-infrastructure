c97f570c6b9606689cb104536ed565c5fa30d6be
dpellicc <dpellicc@everis.com>
2022-10-20 23:05:22 +0200

    IPE-385 add new email api

--- src/domains/idpay-app/04_apim_api_portal.tf
+++ src/domains/idpay-app/04_apim_api_portal.tf
@@ -284,3 +284,45 @@ module "idpay_group_portal" {
   ]
 
 }
+
+## IDPAY Welfare Portal Email API ##
+module "idpay_notification_email_api" {
+  source = "git::https://github.com/pagopa/azurerm.git//api_management_api?ref=v2.18.2"
+
+  name                = "${var.env_short}-idpay-email"
+  api_management_name = data.azurerm_api_management.apim_core.name
+  resource_group_name = data.azurerm_resource_group.apim_rg.name
+
+  description  = "IDPAY Notification Email"
+  display_name = "IDPAY Notification Email API"
+  path         = "idpay/email-notification"
+  protocols    = ["https"]
+
+  service_url = "http://${var.ingress_load_balancer_hostname}/idpaynotificationemail/"
+
+  content_format = "openapi"
+  content_value  = file("./api/idpay_notification_email/openapi.notification.email.yml")
+
+  xml_content = file("./api/base_policy.xml")
+
+  product_ids           = [module.idpay_api_portal_product.product_id]
+  subscription_required = false
+
+  api_operation_policies = [
+    {
+      operation_id = "sendEmail"
+
+      xml_content = templatefile("./api/idpay_notification_email/post-notify-email-policy.xml.tpl", {
+        ingress_load_balancer_hostname = var.ingress_load_balancer_hostname
+      })
+    },
+    {
+      operation_id = "getInstitutionProductUserInfo"
+
+      xml_content = templatefile("./api/idpay_notification_email/get-institution-user-info-policy.xml.tpl", {
+        ingress_load_balancer_hostname = var.ingress_load_balancer_hostname
+      })
+    }
+  ]
+
+}
