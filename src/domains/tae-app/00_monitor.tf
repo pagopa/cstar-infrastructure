798585c2b5e08c633e3011fc86cd21cd42e242e4
TommasoLencioni <tommaso.lencioni@pagopa.it>
2022-10-24 14:38:13 +0200

    [RTD-604] add missing internal ID alert

--- src/domains/tae-app/00_monitor.tf
+++ src/domains/tae-app/00_monitor.tf
@@ -76,19 +76,19 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "sender_doesnt_send" {
   severity             = 0
   criteria {
     query                   = <<-QUERY
-      let today = StorageBlobLogs 
+      let today = StorageBlobLogs
         | where OperationName == 'PutBlob'
         | where TimeGenerated >= ago(1d)
         | extend SenderCode = tostring(extract(@".{126,126}ADE\.([\w\d]{5,5})", 1, Uri))
         | where isnotempty(SenderCode)
         | distinct SenderCode;
-      let yesterday = StorageBlobLogs 
+      let yesterday = StorageBlobLogs
         | where OperationName == 'PutBlob'
         | where TimeGenerated >= ago(2d) and TimeGenerated < ago(1d)
         | extend SenderCode = tostring(extract(@".{126,126}ADE\.([\w\d]{5,5})", 1, Uri))
         | where isnotempty(SenderCode)
         | distinct SenderCode;
-      yesterday 
+      yesterday
         | join kind=leftouter today on SenderCode
         | where isempty(SenderCode1)
         | project SenderCode
@@ -116,6 +116,57 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "sender_doesnt_send" {
   display_name                     = "a-sender-didnt-send"
   enabled                          = true
   #query_time_range_override        = "PT1H"
+  skip_query_validation = false
+  action {
+    action_groups = [azurerm_monitor_action_group.send_to_operations[0].id]
+    custom_properties = {
+      key  = "value"
+      key2 = "value2"
+    }
+  }
+
+  tags = {
+    key = "Sender Monitoring"
+  }
+}
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "sender_auth_missing_internal_id" {
+
+  count = var.env_short == "p" ? 1 : 0
+
+  name                = "cstar-${var.env_short}-sender-auth-missing-internal-id"
+  resource_group_name = data.azurerm_resource_group.monitor_rg.name
+  location            = data.azurerm_resource_group.monitor_rg.location
+
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+  scopes               = [data.azurerm_log_analytics_workspace.log_analytics.id]
+  severity             = 0
+  criteria {
+    query                   = <<-QUERY
+      AppRequests
+      | where TimeGenerated >= ago(5m)
+      | where ClientType != "Browser"
+      | where AppRoleName == "rtdsenderauth"
+      | where OperationName == "GET /sender-code"
+      | where ResultCode == 404
+      QUERY
+    time_aggregation_method = "Count"
+    threshold               = 0
+    operator                = "GreaterThan"
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
+  }
+
+  auto_mitigation_enabled          = false
+  workspace_alerts_storage_enabled = false
+  description                      = "Triggers whenever at least one 404 is returned by Sender Auth in response to a request to obtain sender codes associated to a (missing) internal ID."
+  display_name                     = "cstar-${var.env_short}-sender-auth-missing-internal-id"
+  enabled                          = true
+
   skip_query_validation = false
   action {
     action_groups = [azurerm_monitor_action_group.send_to_operations[0].id]
