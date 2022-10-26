c24c92265ccb956c249bbb9c24cb0f5999f8815e
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2022-10-26 14:43:02 +0200

    feat(monitoring): [DEVOPS-465] Add new alerts channels (#631)
    Add new alerts channels
    
    Co-authored-by: pasqualedevita <>

--- src/aks-platform/99_locals.tf
+++ src/aks-platform/99_locals.tf
@@ -25,4 +25,6 @@ locals {
 
   monitor_action_group_slack_name = "SlackPagoPA"
   monitor_action_group_email_name = "PagoPA"
+  alert_action_group_core_name    = "${var.prefix}${var.env_short}core"
+  alert_action_group_error_name   = "${var.prefix}${var.env_short}error"
 }
