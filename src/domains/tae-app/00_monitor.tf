b7eb7eddace0d4dabb70dbea7716f3b449d7666a
Tommaso Lencioni <tommaso.lencioni@pagopa.it>
2022-11-08 14:53:19 +0100

    feat: [RTD-739] Add aggregates ingestor failure alert (#660)
    * [RTD-739] add alert
    
    * [RTD-739] update readme
    
    * [RTD-739] increase severity

--- src/domains/tae-app/00_monitor.tf
+++ src/domains/tae-app/00_monitor.tf
@@ -728,3 +728,56 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "ack_ingestor_failures
     key = "Sender Monitoring"
   }
 }
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "aggregates_ingestor_failures" {
+
+  count = var.env_short == "p" ? 1 : 0
+
+  name                = "${var.domain}-${var.env_short}-adf-aggregates-ingestor-failures"
+  resource_group_name = data.azurerm_resource_group.monitor_rg.name
+  location            = data.azurerm_resource_group.monitor_rg.location
+
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+  scopes               = [data.azurerm_log_analytics_workspace.log_analytics.id]
+  severity             = 0
+  criteria {
+    query                   = <<-QUERY
+      AzureDiagnostics
+      | where Category == "PipelineRuns"
+      | where pipelineName_s == "aggregates_ingestor"
+      | where OperationName == "aggregates_ingestor - Failed"
+      | where status_s == "Failed"
+      QUERY
+    time_aggregation_method = "Count"
+    threshold               = 0
+    operator                = "GreaterThan"
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
+  }
+
+  auto_mitigation_enabled          = false
+  workspace_alerts_storage_enabled = false
+  description                      = "Triggers whenever at least one aggregates ingestor pipeline fails."
+  display_name                     = "${var.domain}-${var.env_short}-adf-aggregates-ingestor-failures-#ACQ"
+  enabled                          = true
+
+  skip_query_validation = false
+  action {
+    action_groups = [
+      azurerm_monitor_action_group.send_to_operations[0].id,
+      azurerm_monitor_action_group.send_to_zendesk[0].id
+    ]
+    custom_properties = {
+      key  = "value"
+      key2 = "value2"
+    }
+  }
+
+  tags = {
+    key = "Sender Monitoring"
+  }
+}
