3217ae07601eb8f4a9e629e68581476bede929b2
pasqualedevita <>
2022-11-18 12:44:34 +0100

    rollback used

--- src/core/key_vault_0.tf
+++ src/core/key_vault_0.tf
@@ -158,3 +158,23 @@ data "azurerm_key_vault_secret" "cstarblobstorage_public_key" {
   name         = "cstarblobstorage-public-key"
   key_vault_id = module.key_vault.id
 }
+
+#
+# Security Subscription
+#
+data "azurerm_key_vault_secret" "sec_sub_id" {
+  name         = "sec-subscription-id"
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "sec_workspace_id" {
+  count        = var.env_short == "p" ? 1 : 0
+  name         = "sec-workspace-id"
+  key_vault_id = module.key_vault.id
+}
+
+data "azurerm_key_vault_secret" "sec_storage_id" {
+  count        = var.env_short == "p" ? 1 : 0
+  name         = "sec-storage-id"
+  key_vault_id = module.key_vault.id
+}
