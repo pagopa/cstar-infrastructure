79d58bde421997933d7761e26033c75aaa42c0a3
petretiandrea <andrea.petreti@pagopa.it>
2022-12-13 09:20:12 +0100

    fix: [RTD-1032] Restore pm salt (#712)
    * restoring pm salt
    
    * fix "missing" named value on salt policy
    
    * salt pagopa platform for uat all versions
    
    * enabled hashsalt to prod env to api v3

--- src/core/api_rtd.tf
+++ src/core/api_rtd.tf
@@ -120,8 +120,8 @@ module "rtd_payment_instrument_manager" {
       xml_content = templatefile("./api/rtd_payment_instrument_manager/get-hash-salt_policy.xml.tpl", {
         pm-backend-url                       = var.pm_backend_url,
         rtd-pm-client-certificate-thumbprint = data.azurerm_key_vault_secret.rtd_pm_client-certificate-thumbprint.value
-        env_short                            = var.env_short
-        mock_response                        = var.env_short == "d" || var.env_short == "u" || var.env_short == "p"
+        mock_response                        = var.env_short == "d" || var.env_short == "p"
+        pagopa-platform-api-key-name         = azurerm_api_management_named_value.pagopa_platform_api_key[0].display_name
       })
     },
     {
@@ -170,8 +170,8 @@ module "rtd_payment_instrument_manager_v2" {
       xml_content = templatefile("./api/rtd_payment_instrument_manager/get-hash-salt_policy.xml.tpl", {
         pm-backend-url                       = var.pm_backend_url,
         rtd-pm-client-certificate-thumbprint = data.azurerm_key_vault_secret.rtd_pm_client-certificate-thumbprint.value
-        env_short                            = var.env_short
-        mock_response                        = var.env_short == "d" || var.env_short == "u" || var.env_short == "p"
+        mock_response                        = var.env_short == "d" || var.env_short == "p"
+        pagopa-platform-api-key-name         = azurerm_api_management_named_value.pagopa_platform_api_key[count.index].display_name
       })
     },
     {
@@ -204,7 +204,7 @@ module "rtd_payment_instrument_manager_v3" {
   version_set_id      = azurerm_api_management_api_version_set.rtd_payment_instrument_manager.id
   api_version         = "v3"
 
-  depends_on = [module.rtd_payment_instrument_manager]
+  #depends_on = [module.rtd_payment_instrument_manager]
 
   content_value = templatefile("./api/rtd_payment_instrument_manager/swagger.xml.tpl", {
     host = azurerm_api_management_custom_domain.api_custom_domain.proxy[0].host_name
@@ -221,8 +221,8 @@ module "rtd_payment_instrument_manager_v3" {
       xml_content = templatefile("./api/rtd_payment_instrument_manager/get-hash-salt_policy.xml.tpl", {
         pm-backend-url                       = var.pm_backend_url,
         rtd-pm-client-certificate-thumbprint = data.azurerm_key_vault_secret.rtd_pm_client-certificate-thumbprint.value
-        env_short                            = var.env_short
-        mock_response                        = var.env_short == "d" || var.env_short == "u" || var.env_short == "p"
+        mock_response                        = var.env_short == "d",
+        pagopa-platform-api-key-name         = azurerm_api_management_named_value.pagopa_platform_api_key[count.index].display_name
       })
     },
     {
