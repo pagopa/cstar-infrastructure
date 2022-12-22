4ac8d634e0a9276835528194c443ebc12c6ff3cd
Luca Consalvi <luca.consalvi@pagopa.it>
2022-12-22 15:55:12 +0100

    [RTD-1159] added index in rtd_database

--- src/core/rtd_database.tf
+++ src/core/rtd_database.tf
@@ -86,6 +86,11 @@ resource "azurerm_cosmosdb_mongo_collection" "rtd_enrolled_payment_instrument_co
   index {
     keys = ["enabledApps"]
   }
+
+  index {
+    keys = ["hashPanChildren"]
+  }
+
 }
 
 resource "azurerm_cosmosdb_mongo_collection" "sender_auth" {
