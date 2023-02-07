5bf6bdafef23d4bdfc1fad1cb07cb2317428fd95
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-02-07 12:55:55 +0100

    [RTD-598] set decrypter consumer timeout to 10 minutes

--- src/k8s/rtd_configmaps.tf
+++ src/k8s/rtd_configmaps.tf
@@ -65,7 +65,7 @@ resource "kubernetes_config_map" "rtddecrypter" {
     CSV_TRANSACTION_DECRYPT_HOST     = replace(format("apim.internal.%s.cstar.pagopa.it", local.environment_name), "..", ".")
     SPLITTER_LINE_THRESHOLD          = 2000000,
     ENABLE_CHUNK_UPLOAD              = true,
-    CONSUMER_TIMEOUT_MS              = 7200000 # 2h
+    CONSUMER_TIMEOUT_MS              = 600000 # 10m
     },
   var.configmaps_rtddecrypter)
 }
