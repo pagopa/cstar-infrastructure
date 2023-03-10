b15f55f3bdcc9670efcb44608236eb3c1c875f4b
Andrea Morabito <andrea.morabito@pagopa.it>
2023-03-10 11:51:50 +0100

    [RTD-1479] align terraform fqdns

--- src/domains/rtd-common/04_datafactory.tf
+++ src/domains/rtd-common/04_datafactory.tf
@@ -65,5 +65,11 @@ resource "azurerm_data_factory_managed_private_endpoint" "managed_pe" {
   data_factory_id    = azurerm_data_factory.data_factory.id
   target_resource_id = each.key
   subresource_name   = each.value
+
+  lifecycle {
+    ignore_changes = [
+      fqdns
+    ]
+  }
 }
 
