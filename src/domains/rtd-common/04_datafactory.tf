ca75c049d7bbe87306ae3c000c4862a78ca7523f
Andrea Morabito <andrea.morabito@pagopa.it>
2023-03-10 10:46:53 +0100

    [RTD-1479] precommit

--- src/domains/rtd-common/04_datafactory.tf
+++ src/domains/rtd-common/04_datafactory.tf
@@ -65,6 +65,6 @@ resource "azurerm_data_factory_managed_private_endpoint" "managed_pe" {
   data_factory_id    = azurerm_data_factory.data_factory.id
   target_resource_id = each.key
   subresource_name   = each.value[0]
-  fqdns            = [each.value[1]]
+  fqdns              = [each.value[1]]
 }
 
