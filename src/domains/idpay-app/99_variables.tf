04ab695c74256ef211844b067f0ce298dbe3b506
anttorre <antonio.torre@nttdata.com>
2023-04-19 09:52:05 +0200

    fix to UAT var

--- src/domains/idpay-app/99_variables.tf
+++ src/domains/idpay-app/99_variables.tf
@@ -316,10 +316,11 @@ variable "p7m_cert_validity_hours" {
 variable "idpay_mocked_merchant_enable" {
   type        = bool
   description = "Enable mocked merchant APIs"
+  default     = false
 }
 
 variable "idpay_mocked_acquirer_apim_user_id" {
   type        = string
   description = "APIm user id of mocked acquirer"
-  default     = null
+  default     = ""
 }
