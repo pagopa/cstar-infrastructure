0ee10487935ceb7ea5d629e4f6e4a26e36d95af9
Luca Consalvi <luca.consalvi@pagopa.it>
2023-04-19 14:33:56 +0200

    [RTD-1623] add api product for payment instrument

--- src/domains/rtd-app/04_apim_api_pi.tf
+++ src/domains/rtd-app/04_apim_api_pi.tf
@@ -1,14 +1,14 @@
 #
 # New Payment Gateway PRODUCTS
 #
-module "npg_api_product" {
+module "pi_api_product" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v3.0.0"
 
   count = var.enable.api_payment_instrument ? 1 : 0
 
-  product_id   = "ngp_api_product"
-  display_name = "NPG_API_PRODUCT"
-  description  = "NPG_API_PRODUCT"
+  product_id   = "pi_api_product"
+  display_name = "PI_API_PRODUCT"
+  description  = "PI_API_PRODUCT"
 
   api_management_name = data.azurerm_api_management.apim_core.name
   resource_group_name = data.azurerm_resource_group.apim_rg.name
@@ -19,31 +19,31 @@ module "npg_api_product" {
 
   subscriptions_limit = 0
 
-  policy_xml = templatefile("./api_product/npg/policy.xml", {})
+  policy_xml = templatefile("./api_product/payment_instrument/policy.xml", {})
 
 }
 
 
 ## NPG Payment Instrument registrtion API ##
-module "npg_payment_instrument" {
+module "payment_instrument" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v3.0.0"
 
   count = var.enable.api_payment_instrument ? 1 : 0
 
-  name                = "${var.env_short}-npg-payment-instrument"
+  name                = "${var.env_short}-payment-instrument"
   api_management_name = data.azurerm_api_management.apim_core.name
   resource_group_name = data.azurerm_resource_group.apim_rg.name
 
-  description  = "NPG Payment Instrument registrtion"
-  display_name = "NPG Payment Instrument registrtion API"
+  description  = "Payment Instrument interaction"
+  display_name = "Payment Instrument interaction API"
 
-  path      = "npg/paymentinstrument"
+  path      = "paymentinstrument"
   protocols = ["https"]
 
   service_url = "https://${var.ingress_load_balancer_hostname}/paymentinstrument"
 
   content_format = "openapi"
-  content_value  = templatefile("./api/npg_payment_instrument/openapi.npg_payment_instrument.yml", {})
+  content_value  = templatefile("./api/payment_instrument/openapi.payment_instrument.yml", {})
 
   xml_content = file("./api/base_policy.xml")
 
