375e2a678d44c5cf16a6802dd0ee29e06896584b
Giovanni <g.mancini.0@gmail.com>
2023-04-28 16:35:37 +0200

    Fix pair peering

--- src/core/network_pair.tf
+++ src/core/network_pair.tf
@@ -43,11 +43,11 @@ module "vnet_peering_pair_vs_integration" {
   source_resource_group_name       = azurerm_resource_group.rg_pair_vnet.name
   source_virtual_network_name      = module.vnet_pair.name
   source_remote_virtual_network_id = module.vnet_pair.id
-  source_allow_gateway_transit     = false
-  source_use_remote_gateways       = true
+  source_allow_gateway_transit     = true
+  source_use_remote_gateways       = false
   # needed by vpn gateway for enabling routing from vnet to vnet_integration
   target_resource_group_name       = azurerm_resource_group.rg_vnet.name
   target_virtual_network_name      = module.vnet_integration.name
   target_remote_virtual_network_id = module.vnet_integration.id
-  target_use_remote_gateways       = true # needed by vnet peering with SIA
+  target_use_remote_gateways       = false # needed by vnet peering with SIA
 }
