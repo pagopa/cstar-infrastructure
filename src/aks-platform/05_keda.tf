dea636aa40c578c438440f3d75576b8e86b4b2b0
Luca Consalvi <117908483+lucaconsalvi@users.noreply.github.com>
2023-05-09 11:29:38 +0200

    chore: [RTD-1537] switch terraform-azurerm-v3 core (#885)
    * [RTD-1537] change from azurerm v2 to azurerm v3 for core
    
    * [RTD-1537] core fixes
    
    * [RTD-1537] Use azurerm 6.2.1 v3 on aks
    
    * [RTD-1537] switch to last repo version
    
    * [RTD-1537] Fix dependencies and re-import aks state
    
    * [RTD-1537] fix routes priority
    
    * [RTD-1537] reduce some updates
    
    * [RTD-1537] reduce add on plan
    
    * Run precommit check
    
    * Fix postgres dns private zone
    
    * breaking: Created DNS forwarderd in northeurope for DR (#900)
    
    * Fix pair peering
    
    * Fix observability config flexible server pgres
    
    * fix cirds for pair vnet
    
    * added pair vnet peering to aks vnet
    
    * minor fix
    
    * keep sftp enabled
    
    * Modify replication type to RAGZRS of acquirer storage account (only in prod)
    
    * ddos_protection_plan: temporany removed
    
    * Fix dns forwarder module version
    
    * Run precommit check
    
    ---------
    
    Co-authored-by: Giovanni <g.mancini.0@gmail.com>
    Co-authored-by: Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
    Co-authored-by: diegoitaliait <diego.lagosmorales@pagopa.it>

--- src/aks-platform/05_keda.tf
+++ src/aks-platform/05_keda.tf
@@ -13,7 +13,7 @@ locals {
 }
 
 module "keda_pod_identity" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_pod_identity?ref=v4.1.9"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_pod_identity?ref=v6.2.1"
 
   resource_group_name = azurerm_resource_group.rg_aks.name
   location            = var.location
