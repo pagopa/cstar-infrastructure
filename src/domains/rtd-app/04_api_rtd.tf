8ed12130daab293e07a642ed0d3a723e12d3883e
Luca Consalvi <luca.consalvi@pagopa.it>
2023-05-17 17:54:22 +0200

    [RTD-1676] switch from rtd_api_product_internal module to officail resource

--- src/domains/rtd-app/04_api_rtd.tf
+++ src/domains/rtd-app/04_api_rtd.tf
@@ -23,24 +23,28 @@ resource "azurerm_api_management_product_policy" "rtd_api_product" {
   xml_content = file("./api_product/rtd_api/policy.xml")
 }
 
-
-module "rtd_api_product_internal" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_product?ref=v6.2.1"
+resource "azurerm_api_management_product" "rtd_api_product_internal" {
+  api_management_name = data.azurerm_api_management.apim_core.name
+  resource_group_name = data.azurerm_resource_group.apim_rg.name
 
   product_id   = "rtd-api-product-internal"
   display_name = "RTD_API_Product Internal"
   description  = "RTD_API_Product Internal"
 
+  subscription_required = true
+  subscriptions_limit   = 5
+  approval_required     = true
+  published             = true
+
+}
+
+resource "azurerm_api_management_product_policy" "rtd_api_product_internal" {
+
+  product_id          = azurerm_api_management_product.rtd_api_product_internal.product_id
   api_management_name = data.azurerm_api_management.apim_core.name
   resource_group_name = data.azurerm_resource_group.apim_rg.name
 
-  published             = true
-  subscription_required = true
-  approval_required     = true
-
-  subscriptions_limit = 5
-
-  policy_xml = templatefile("./api_product/rtd_api_internal/policy.xml.tpl", {
+  xml_content = templatefile("./api_product/rtd_api_internal/policy.xml.tpl", {
     k8s-cluster-ip-range-from     = var.k8s_ip_filter_range.from
     k8s-cluster-ip-range-to       = var.k8s_ip_filter_range.to
     k8s-cluster-aks-ip-range-from = var.k8s_ip_filter_range_aks.from
