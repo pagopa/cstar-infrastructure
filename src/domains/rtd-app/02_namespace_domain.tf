52172955a0ebeb1b1e0dff55e758f13d78fb6946
Luca Consalvi <luca.consalvi@pagopa.it>
2023-05-22 16:24:07 +0200

    [RTD-1676] fixes

--- src/domains/rtd-app/02_namespace_domain.tf
+++ src/domains/rtd-app/02_namespace_domain.tf
@@ -4,56 +4,19 @@ resource "kubernetes_namespace" "domain_namespace" {
   }
 }
 
-resource "azurerm_user_assigned_identity" "this" {
+module "domain_pod_identity" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3//kubernetes_pod_identity?ref=v6.3.1"
+
   resource_group_name = local.aks_resource_group_name
   location            = var.location
+  tenant_id           = data.azurerm_subscription.current.tenant_id
+  cluster_name        = local.aks_name
 
-  name = "${var.domain}-pod-identity"
-}
+  identity_name = "${var.domain}-pod-identity"
+  namespace     = kubernetes_namespace.domain_namespace.metadata[0].name
+  key_vault_id  = data.azurerm_key_vault.kv.id
 
-resource "azurerm_key_vault_access_policy" "this" {
-
-  key_vault_id = data.azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_subscription.current.tenant_id
-
-  # The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault.
-  object_id = azurerm_user_assigned_identity.this.principal_id
-
-  certificate_permissions = []
-  key_permissions         = []
-  secret_permissions      = ["Get"]
-}
-
-resource "null_resource" "create_pod_identity" {
-  triggers = {
-    resource_group = local.aks_resource_group_name
-    cluster_name   = local.aks_name
-    namespace      = kubernetes_namespace.domain_namespace.metadata[0].name
-    name           = "${var.domain}-pod-identity"
-    identity_id    = azurerm_user_assigned_identity.this.id
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      az aks pod-identity add \
-        --resource-group ${self.triggers.resource_group} \
-        --cluster-name ${self.triggers.cluster_name} \
-        --namespace ${self.triggers.namespace} \
-        --name ${self.triggers.name} \
-        --identity-resource-id ${self.triggers.identity_id}
-    EOT
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-      az aks pod-identity delete \
-        --resource-group ${self.triggers.resource_group} \
-        --cluster-name ${self.triggers.cluster_name} \
-        --namespace ${self.triggers.namespace} \
-        --name ${self.triggers.name}
-    EOT
-  }
+  secret_permissions = ["Get"]
 }
 
 resource "helm_release" "reloader" {
