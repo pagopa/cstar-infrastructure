3f19dae345fc91af3954c41cb22d1e1fcfb6d2be
mamari90 <130982006+mamari90@users.noreply.github.com>
2023-05-30 16:51:52 +0200

    fix version

--- src/domains/tae-app/05_aks_middleware_tools.tf
+++ src/domains/tae-app/05_aks_middleware_tools.tf
@@ -1,5 +1,5 @@
 module "cert_mounter" {
-  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cert_mounter?ref=6.14.0"
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cert_mounter?ref=v6.14.0"
   namespace           = var.domain
   certificate_name    = "${var.aks_cluster_domain_name}-${var.domain}-internal-${var.env}-cstar-pagopa-it"
   kv_name             = data.azurerm_key_vault.kv.name
