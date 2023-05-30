eced9b9af6ba1c5f5513d84472fdd8b22aa6a4bd
mamari90 <130982006+mamari90@users.noreply.github.com>
2023-05-30 10:55:48 +0200

    updated module version

--- src/domains/rtd-app/80_tls_checker.tf
+++ src/domains/rtd-app/80_tls_checker.tf
@@ -18,7 +18,7 @@ module "tls_checker" {
 
 
 module "cert_mounter" {
-  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cert_mounter?ref=050ec6f"
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cert_mounter?ref=v6.14.0"
   namespace           = var.domain
   certificate_name    = "${var.aks_cluster_domain_name}-${var.domain}-internal-${var.env}-cstar-pagopa-it"
   kv_name             = data.azurerm_key_vault.kv.name
