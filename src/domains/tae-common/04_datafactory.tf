fc71fb896be300ca200381df4f0c1bfee5726995
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-06-10 15:11:17 +0200

    datafactory: fix fqdn non necessary for kusto private connection

--- src/domains/tae-common/04_datafactory.tf
+++ src/domains/tae-common/04_datafactory.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "data_factory_rg" {
-  name     = format("%s-df-rg", local.project)
+  name     = "${local.project}-df-rg"
   location = var.location
   tags     = var.tags
 }
 
 resource "azurerm_data_factory" "data_factory" {
-  name                   = format("%s-df", local.project)
+  name                   = "${local.project}-df"
   location               = azurerm_resource_group.data_factory_rg.location
   resource_group_name    = azurerm_resource_group.data_factory_rg.name
   public_network_enabled = false
@@ -22,7 +22,7 @@ resource "azurerm_data_factory" "data_factory" {
 
 resource "azurerm_data_factory_integration_runtime_azure" "autoresolve" {
   name                    = "AutoResolveIntegrationRuntime"
-  resource_group_name     = azurerm_resource_group.data_factory_rg.name
+  # resource_group_name     = azurerm_resource_group.data_factory_rg.name
   data_factory_id         = azurerm_data_factory.data_factory.id
   location                = "AutoResolve"
   virtual_network_enabled = true
@@ -30,18 +30,18 @@ resource "azurerm_data_factory_integration_runtime_azure" "autoresolve" {
 
 resource "azurerm_private_endpoint" "data_factory_pe" {
 
-  name                = format("%s-pe", azurerm_data_factory.data_factory.name)
+  name                = "${azurerm_data_factory.data_factory.name}-pe"
   location            = azurerm_resource_group.data_factory_rg.location
   resource_group_name = azurerm_resource_group.data_factory_rg.name
   subnet_id           = data.azurerm_subnet.private_endpoint_snet.id
 
   private_dns_zone_group {
-    name                 = format("%s-private-dns-zone-group", azurerm_data_factory.data_factory.name)
+    name                 = "${azurerm_data_factory.data_factory.name}-private-dns-zone-group"
     private_dns_zone_ids = [data.azurerm_private_dns_zone.adf.id]
   }
 
   private_service_connection {
-    name                           = format("%s-private-service-connection", azurerm_data_factory.data_factory.name)
+    name                           = "${azurerm_data_factory.data_factory.name}-private-service-connection"
     private_connection_resource_id = azurerm_data_factory.data_factory.id
     is_manual_connection           = false
     subresource_names              = ["datafactory"]
@@ -64,17 +64,18 @@ resource "azurerm_private_dns_a_record" "data_factory_a_record" {
 resource "azurerm_data_factory_managed_private_endpoint" "managed_pe" {
   for_each = tomap(
     {
-      (data.azurerm_storage_account.acquirer_sa.id)   = ["blob", format("cstar%sblobstorage.blob.core.windows.net", var.env_short)],
-      (data.azurerm_storage_account.sftp_sa.id)       = ["blob", format("cstar%ssftp.blob.core.windows.net", var.env_short)],
-      (module.cosmosdb_account.id)                    = ["SQL", format("cstar-%s-weu-tae-cosmos-db-account.documents.azure.com", var.env_short)],
-      (data.azurerm_kusto_cluster.dexp_cluster[0].id) = ["cluster", format("cstar%sdataexplorer.westeurope.kusto.windows.net", var.env_short)]
+      (data.azurerm_storage_account.acquirer_sa.id)   = ["blob", "cstar${var.env_short}blobstorage.blob.core.windows.net"],
+      (data.azurerm_storage_account.sftp_sa.id)       = ["blob", "cstar${var.env_short}sftp.blob.core.windows.net"],
+      (module.cosmosdb_account.id)                    = ["SQL", "cstar-${var.env_short}-weu-tae-cosmos-db-account.documents.azure.com"],
+      (data.azurerm_kusto_cluster.dexp_cluster[0].id) = ["cluster", ""]
     }
   )
   name               = replace(format("%s-%s-mng-private-endpoint", azurerm_data_factory.data_factory.name, substr(sha256(each.key), 0, 3)), "-", "_")
   data_factory_id    = azurerm_data_factory.data_factory.id
   target_resource_id = each.key
   subresource_name   = each.value[0]
-  fqdns              = [each.value[1]]
+  # fqdns              = [each.value[1]]
+  fqdns              = each.value[1] == "" ? null : [each.value[1]]
 }
 
 resource "azurerm_role_assignment" "adf_data_contributor_role_on_sa" {
@@ -90,7 +91,7 @@ resource "azurerm_role_assignment" "adf_data_contributor_role_on_sftp" {
 }
 
 resource "azurerm_role_definition" "adf_pipeline_run_role" {
-  name        = format("%s-df-run-pipeline-role", local.project)
+  name        = "${local.project}-df-run-pipeline-role"
   scope       = azurerm_data_factory.data_factory.id
   description = "ADF RunPipeline Role created via Terraform"
 
