46ed2d9cb9a1022ce0daf9ee559bb1617c7aee9f
Luca Consalvi <luca.consalvi@pagopa.it>
2023-06-16 09:43:23 +0200

    [RTD-1704] alert for incident

--- src/domains/tae-app/00_monitor_incident.tf
+++ src/domains/tae-app/00_monitor_incident.tf
@@ -78,3 +78,73 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "cstar-ade-in-missing-
   }
 }
 
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "cstar-external-access-problems" {
+
+  count = var.env_short == "p" ? 1 : 0
+
+  name                = "tae-${var.env_short}-external-access-problems"
+  resource_group_name = data.azurerm_resource_group.monitor_rg.name
+  location            = data.azurerm_resource_group.monitor_rg.location
+
+  evaluation_frequency = "PT6H"
+  window_duration      = "P1D"
+  scopes               = [data.azurerm_log_analytics_workspace.log_analytics.id]
+  severity             = 0
+
+  auto_mitigation_enabled          = false
+  workspace_alerts_storage_enabled = false
+  description                      = "The alarm will be triggered when more then 4 senders have had problems to interact with our API."
+  display_name                     = "tae-${var.env_short}-external-access-problems#INC"
+  enabled                          = true
+
+  skip_query_validation = false
+
+  action {
+    action_groups = [
+      data.azurerm_monitor_action_group.slack.id # cstar-status
+    ]
+  }
+
+  criteria {
+    time_aggregation_method = "Count"
+    threshold               = 4
+    operator                = "GreaterThanOrEqual"
+    query                   = <<-QUERY
+                  let monitor_api_file_register_sender_ade_ack = AzureDiagnostics
+                      | where url_s has "file-register/sender-ade-ack"
+                      | where isRequestSuccess_b == "false"
+                      | project GeneralId=tostring(apimSubscriptionId_s);
+                  let monitor_api_rtd_csv_transaction = AzureDiagnostics
+                      | where url_s has "https://api.cstar.pagopa.it/rtd/csv-transaction"
+                      | where url_s has_any("/sas", "/publickey")
+                      | where isRequestSuccess_b == "false"
+                      | project GeneralId=tostring(apimSubscriptionId_s);
+                  let monitor_api_ade = AzureDiagnostics
+                    | where url_s has "https://api.cstar.pagopa.it/ade"
+                      | where isRequestSuccess_b == "false"
+                      | project GeneralId=tostring(apimSubscriptionId_s);
+                  let monitor_api_pagopastorage = AzureDiagnostics
+                      | where url_s has "https://api.cstar.pagopa.it/pagopastorage"
+                      | where isRequestSuccess_b == "false"
+                      | project GeneralId=tostring(apimSubscriptionId_s);
+
+                  union 
+                      monitor_api_rtd_csv_transaction,
+                      monitor_api_pagopastorage, 
+                      monitor_api_file_register_sender_ade_ack, 
+                      monitor_api_ade
+                  | where GeneralId != ""
+                  | distinct GeneralId
+                  QUERY
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
+  }
+
+  tags = {
+    key = "Incident Alert"
+  }
+}
+
