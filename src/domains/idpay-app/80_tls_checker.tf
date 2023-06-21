b3ef5fd957d7d4b684405cd7781c9e58572fffe7
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-06-21 18:09:52 +0200

    fix idpay and rtd cert mounter cert name

--- src/domains/idpay-app/80_tls_checker.tf
+++ src/domains/idpay-app/80_tls_checker.tf
@@ -23,7 +23,7 @@ module "tls_checker" {
 module "cert_mounter" {
   source           = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cert_mounter?ref=v6.15.2"
   namespace        = var.domain
-  certificate_name = "${var.aks_cluster_domain_name}-${var.domain}-internal-${var.env}-cstar-pagopa-it"
+  certificate_name = var.env_short == "p" ? "${var.aks_cluster_domain_name}-${var.domain}-internal-cstar-pagopa-it" : "${var.aks_cluster_domain_name}-${var.domain}-internal-${var.env}-cstar-pagopa-it"
   kv_name          = data.azurerm_key_vault.kv.name
   tenant_id        = data.azurerm_subscription.current.tenant_id
 }
