1efe886986cfca0fd2c481f2097c1e6e640153cb
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-07-03 16:31:24 +0200

    test

--- src/domains/rtd-common/_deleteme.tf
+++ src/domains/rtd-common/_deleteme.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "echo_date" {
   provisioner "local-exec" {
     command = <<-EOT
-      echo "La data rren è $(date)"
+      echo "La data ccsdrren è $(date)"
     EOT
   }
 }
