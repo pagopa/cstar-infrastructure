9f8b9441a0f561c2d94f865e433a74356d79c3eb
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-07-03 15:20:35 +0200

    try

--- src/domains/rtd-common/_deleteme.tf
+++ src/domains/rtd-common/_deleteme.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "echo_date" {
   provisioner "local-exec" {
     command = <<-EOT
-      echo "La data corrente è $(date)"
+      echo "La data corren è $(date)"
     EOT
   }
 }
