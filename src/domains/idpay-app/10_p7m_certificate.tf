463053626a21e8b1dcc71f8fb2626655c5ae2108
Luca Consalvi <luca.consalvi@pagopa.it>
2023-07-05 11:48:04 +0200

    [RTD-1793] add ignore changes for expiration date

--- src/domains/idpay-app/10_p7m_certificate.tf
+++ src/domains/idpay-app/10_p7m_certificate.tf
@@ -32,21 +32,33 @@ resource "azurerm_key_vault_secret" "ranking_p7m_private_key" {
   name            = "ranking-p7m-key"
   value           = tls_private_key.p7m_key[0].private_key_pem
   content_type    = "text/plain"
-  expiration_date = replace(formatdate("YYYY-MM-DD'T'hh:mm:ssZ", timeadd(tls_self_signed_cert.p7m_self_signed_cert[0].validity_end_time, "-59m59s500ms")), "+01:00", "Z")
+  expiration_date = tls_self_signed_cert.p7m_self_signed_cert[0].validity_end_time
 
   key_vault_id = data.azurerm_key_vault.kv.id
+
+  lifecycle {
+    ignore_changes = [
+      expiration_date
+    ]
+  }
+
 }
 
 #tfsec:ignore:azure-keyvault-ensure-secret-expiry
 resource "azurerm_key_vault_secret" "ranking_p7m_cert" {
   count = var.enable_p7m_self_sign ? 1 : 0
 
-  name         = "ranking-p7m-cert"
-  value        = tls_self_signed_cert.p7m_self_signed_cert[0].cert_pem
-  content_type = "text/plain"
-  #expiration_date = tls_self_signed_cert.p7m_self_signed_cert[0].validity_end_time
-  expiration_date = replace(formatdate("YYYY-MM-DD'T'hh:mm:ssZ", timeadd(tls_self_signed_cert.p7m_self_signed_cert[0].validity_end_time, "-59m59s500ms")), "+01:00", "Z")
+  name            = "ranking-p7m-cert"
+  value           = tls_self_signed_cert.p7m_self_signed_cert[0].cert_pem
+  content_type    = "text/plain"
+  expiration_date = tls_self_signed_cert.p7m_self_signed_cert[0].validity_end_time
 
   key_vault_id = data.azurerm_key_vault.kv.id
+
+  lifecycle {
+    ignore_changes = [
+      expiration_date
+    ]
+  }
 
 }
