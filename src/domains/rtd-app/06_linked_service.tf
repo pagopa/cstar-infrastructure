539e1850a18319049bb2feb11b1c10da48490efb
Giovanni <g.mancini.0@gmail.com>
2023-07-25 12:16:10 +0200

    fix: Remove useless linked services (#1052)
    Remove useless linked services

--- src/domains/rtd-app/06_linked_service.tf
+++ src/domains/rtd-app/06_linked_service.tf
@@ -1,19 +1,7 @@
-data "azurerm_key_vault_secret" "mongodb_connection_uri" {
-  name         = "mongo-db-connection-uri"
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
-
-resource "azurerm_data_factory_linked_service_cosmosdb_mongoapi" "cosmos_linked_service" {
-  name            = "${local.product}-cosmos-linked-service"
-  data_factory_id = data.azurerm_data_factory.datafactory.id
-
-  database = "rtd"
-
-  connection_string              = data.azurerm_key_vault_secret.mongodb_connection_uri.value
-  server_version_is_32_or_higher = true
-}
-
+# Used by BPD pipelines
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "storage_account_linked_service" {
+
+  count = var.env_short == "p" ? 1 : 0
 
   name            = "${local.product}-sa-linked-service"
   data_factory_id = data.azurerm_data_factory.datafactory.id
