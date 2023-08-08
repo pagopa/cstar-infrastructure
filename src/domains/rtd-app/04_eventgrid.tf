bbe4065559c9149fb1b2cfb8b2aac45c64eb50fc
petretiandrea <andrea.petreti@pagopa.it>
2023-08-08 10:11:50 +0200

    chore: [RTD-1872] Create storage subscription for eventgrid (#1086)
    * create storage subscription for eventgrid
    
    * add sftp event grid subscription
    
    * fix wrong feature flag disabled
    
    * moved after chaning count
    
    ---------
    
    Co-authored-by: Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>

---
+++ src/domains/rtd-app/04_eventgrid.tf
@@ -0,0 +1,79 @@
+data "azurerm_eventhub" "rtd_platform_eventhub" {
+  name                = "rtd-platform-events"
+  resource_group_name = data.azurerm_resource_group.msg_rg.name
+  namespace_name      = data.azurerm_eventhub_namespace.event_hub_rtd.name
+  depends_on = [
+    azurerm_eventhub.event_hub_rtd_hub
+  ]
+}
+
+# Blob storage evengrid to platform-events
+data "azurerm_eventgrid_system_topic" "storage_topic" {
+  name                = "${local.product}-events-storage-topic"
+  resource_group_name = data.azurerm_resource_group.rg_storage.name
+}
+
+resource "azurerm_eventgrid_system_topic_event_subscription" "storage_subscription" {
+  count               = var.enable.blob_storage_event_grid_integration && var.env_short != "p" ? 1 : 0
+  name                = "${local.project}-events-storage-subscription"
+  system_topic        = data.azurerm_eventgrid_system_topic.storage_topic.name
+  resource_group_name = data.azurerm_resource_group.rg_storage.name
+
+  eventhub_endpoint_id = data.azurerm_eventhub.rtd_platform_eventhub.id
+
+  delivery_identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_role_assignment.event_grid_sender_role_on_rtd_platform_events
+  ]
+}
+
+# Assign role to event grid topic storage-topic to publish over rtd-platform-events
+resource "azurerm_role_assignment" "event_grid_sender_role_on_rtd_platform_events" {
+  count                = var.enable.blob_storage_event_grid_integration && var.env_short != "p" ? 1 : 0
+  role_definition_name = "Azure Event Hubs Data Sender"
+  principal_id         = data.azurerm_eventgrid_system_topic.storage_topic.identity[0].principal_id
+  scope                = data.azurerm_eventhub.rtd_platform_eventhub.id
+
+  depends_on = [
+    azurerm_eventhub.event_hub_rtd_hub
+  ]
+}
+
+# SFTP storage eventgrid to platform-events
+data "azurerm_eventgrid_system_topic" "sftp" {
+  name                = "${local.product}-sftp-topic"
+  resource_group_name = "${local.product}-sftp-rg"
+}
+
+resource "azurerm_eventgrid_system_topic_event_subscription" "sftp" {
+  count                = var.enable.blob_storage_event_grid_integration && var.env_short != "p" ? 1 : 0
+  name                 = "${local.project}-sftp-subscription"
+  system_topic         = data.azurerm_eventgrid_system_topic.sftp.name
+  resource_group_name  = "${local.product}-sftp-rg"
+  eventhub_endpoint_id = data.azurerm_eventhub.rtd_platform_eventhub.id
+  subject_filter {
+    subject_begins_with = "/blobServices/default/containers/ade/blobs/"
+  }
+
+  delivery_identity {
+    type = "SystemAssigned"
+  }
+
+  depends_on = [
+    azurerm_role_assignment.event_grid_sender_role_sftp_on_rtd_platform_events
+  ]
+}
+
+# Assign role to event grid topic sftp to publish over rtd-platform-events
+resource "azurerm_role_assignment" "event_grid_sender_role_sftp_on_rtd_platform_events" {
+  count                = var.enable.blob_storage_event_grid_integration && var.env_short != "p" ? 1 : 0
+  role_definition_name = "Azure Event Hubs Data Sender"
+  principal_id         = data.azurerm_eventgrid_system_topic.sftp.identity[0].principal_id
+  scope                = data.azurerm_eventhub.rtd_platform_eventhub.id
+  depends_on = [
+    azurerm_eventhub.event_hub_rtd_hub
+  ]
+}
