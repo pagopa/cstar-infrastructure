cc1a20b6c899573ec3face79534b0afca473e8e2
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-09-11 13:26:05 +0200

    moved dns privatelink eventhub for idpay-common

--- src/domains/idpay-common/00_dns_private.tf
+++ src/domains/idpay-common/00_dns_private.tf
@@ -1,6 +1,6 @@
 data "azurerm_private_dns_zone" "ehub" {
   name                = "privatelink.servicebus.windows.net"
-  resource_group_name = "${local.product}-msg-rg"
+  resource_group_name = data.azurerm_virtual_network.vnet_core.resource_group_name
 }
 
 data "azurerm_private_dns_zone" "redis" {
