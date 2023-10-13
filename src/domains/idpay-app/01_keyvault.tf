ab1abe211fd9aa16da4437339767186e9a532fd4
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2023-10-13 11:29:54 +0200

    feat: Idp 1914 idpay code keys (#1154)
    * [IDP-1919] key exposition
    
    * IDP-1914
    mil and idpay RSA key generation
    
    ---------
    
    Co-authored-by: frtrinca <francesco.trinca@nttdata.com>
    Co-authored-by: Tommaso Lencioni <tommaso.lencioni@pagopa.it>

--- src/domains/idpay-app/01_keyvault.tf
+++ src/domains/idpay-app/01_keyvault.tf
@@ -42,3 +42,25 @@ data "azurerm_key_vault_secret" "alert-slack-idpay" {
   name         = "alert-idpay-notification-slack"
   key_vault_id = data.azurerm_key_vault.kv.id
 }
+
+resource "azurerm_key_vault_key" "idpay-mil-key" {
+  name         = "idpay-mil-key"
+  key_vault_id = data.azurerm_key_vault.kv.id
+  key_type     = "RSA"
+  key_size     = 2048
+  key_opts = [
+    "decrypt",
+    "encrypt"
+  ]
+}
+
+resource "azurerm_key_vault_key" "idpay-pinblock-key" {
+  name         = "idpay-pinblock-key"
+  key_vault_id = data.azurerm_key_vault.kv.id
+  key_type     = "RSA"
+  key_size     = 2048
+  key_opts = [
+    "decrypt",
+    "encrypt"
+  ]
+}
