717618ef965568374cc86c9eb81d7691833cb638
frtrinca <106818915+frtrinca@users.noreply.github.com>
2023-10-27 14:04:41 +0200

    feat: [IDP-1991] generic API for transaction (#1159)
    * [IDP-1991] generic API for transaction
    
    * [IDP-1991] fix check diff
    
    * [IDP-1991] add enum to payment exception
    
    * [IDP-1991] remove deprecated API MIL
    
    * [IDP-1991] remove deprecated API MIL, fix precommit
    
    * [IDP-1991] add payment exception
    
    * [IDP-1985] fix check diff
    
    * Revert "[IDP-1985] fix check diff"
    
    This reverts commit cfbbce464a289f43e364b56ea179d9b4f7ec07e8.
    
    * [IDP-1991] update description for min.int and mil payment
    
    * [IDP-1991] fixed swagger mil and fragment merchant id retriever
    
    ---------
    
    Co-authored-by: stedelia <stefano11.delia@nttdata.com>
    Co-authored-by: Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
    Co-authored-by: Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>

--- src/domains/idpay-app/04_apim_api_mil.tf
+++ src/domains/idpay-app/04_apim_api_mil.tf
@@ -23,48 +23,6 @@ module "idpay_api_mil_product" {
   groups = ["developers"]
 }
 
-## IDPAY MIL API ##
-module "idpay_mil" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.15.2"
-
-  name                = "${var.env_short}-idpay-mil"
-  api_management_name = data.azurerm_api_management.apim_core.name
-  resource_group_name = data.azurerm_resource_group.apim_rg.name
-
-  description  = "IDPAY MIL"
-  display_name = "IDPAY MIL API"
-  path         = "idpay/mil/payment/qr-code/merchant"
-  protocols    = ["https"]
-
-  service_url = "${local.ingress_load_balancer_https}/idpaypayment/idpay/payment/qr-code/merchant"
-
-  content_format = "openapi"
-  content_value  = file("./api/idpay_mil/openapi.mil.yml")
-
-  xml_content = file("./api/base_policy.xml")
-
-  product_ids           = [module.idpay_api_mil_product.product_id]
-  subscription_required = true
-
-  api_operation_policies = [
-    {
-      operation_id = "getMerchantInitiativeList"
-
-      xml_content = templatefile("./api/idpay_mil/get-merchant-initiatives-policy.xml.tpl", {
-        ingress_load_balancer_hostname = var.ingress_load_balancer_hostname
-      })
-    },
-    {
-      operation_id = "uploadMerchantList"
-
-      xml_content = templatefile("./api/idpay_mil/put-merchant-upload-policy.xml.tpl", {
-        ingress_load_balancer_hostname = var.ingress_load_balancer_hostname
-      })
-    }
-  ]
-
-}
-
 ## IDPAY MIL PAYMENT API ##
 module "idpay_mil_payment" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.15.2"
@@ -102,6 +60,20 @@ module "idpay_mil_payment" {
       xml_content = templatefile("./api/idpay_mil/idpay_mil_payment/get-public-key-policy.xml.tpl", {
         idpay-mil-key = azurerm_key_vault_key.idpay-mil-key.name
         keyvault-name = data.azurerm_key_vault.kv.name
+      })
+    },
+    {
+      operation_id = "getStatusTransaction"
+
+      xml_content = templatefile("./api/idpay_mil/idpay_mil_payment/get-transaction-policy.xml.tpl", {
+        ingress_load_balancer_hostname = var.ingress_load_balancer_hostname
+      })
+    },
+    {
+      operation_id = "deleteTransaction"
+
+      xml_content = templatefile("./api/idpay_mil/idpay_mil_payment/delete-transaction-policy.xml.tpl", {
+        ingress_load_balancer_hostname = var.ingress_load_balancer_hostname
       })
     }
   ]
