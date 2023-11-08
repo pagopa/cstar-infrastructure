1e846e00d739542c87abfbf5b2fced5d40e95c7f
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2023-11-08 11:00:51 +0100

    [IDP-2027] added policy token test for merchants portal

--- src/domains/idpay-app/04_apim_api_token_exchange_merchants.tf
+++ src/domains/idpay-app/04_apim_api_token_exchange_merchants.tf
@@ -94,6 +94,34 @@ resource "azurerm_api_management_api_operation_policy" "idpay_merchants_token_ex
     origins                     = local.origins.base
   })
 
-  depends_on = [azapi_resource.apim-merchant-id-retriever, null_resource.upload_oidc_configuration]
+  depends_on = [
+    azapi_resource.apim-merchant-id-retriever,
+    null_resource.upload_oidc_configuration
+  ]
+}
+
+resource "azurerm_api_management_api_operation" "idpay_merchants_token_exchange_test" {
+  count               = var.env_short != "p" ? 1 : 0
+  operation_id        = "idpay-token-exchange-merchants_test"
+  api_name            = azurerm_api_management_api.idpay_merchants_token_exchange.name
+  api_management_name = data.azurerm_api_management.apim_core.name
+  resource_group_name = data.azurerm_resource_group.apim_rg.name
+  display_name        = "IDPAY Token Exchange for Merchants Portal test"
+  method              = "POST"
+  url_template        = "/test"
+  description         = "Endpoint for selfcare token exchange towards merchants portal test"
+}
+
+resource "azurerm_api_management_api_operation_policy" "idpay_merchants_token_exchange_policy_test" {
+  count               = var.env_short != "p" ? 1 : 0
+  api_name            = azurerm_api_management_api_operation.idpay_merchants_token_exchange_test[0].api_name
+  api_management_name = azurerm_api_management_api_operation.idpay_merchants_token_exchange_test[0].api_management_name
+  resource_group_name = azurerm_api_management_api_operation.idpay_merchants_token_exchange_test[0].resource_group_name
+  operation_id        = azurerm_api_management_api_operation.idpay_merchants_token_exchange_test[0].operation_id
+
+  xml_content = templatefile("./api/idpay_token_exchange/jwt_merchants_token_test.xml.tpl", {
+    ingress_load_balancer_hostname = var.ingress_load_balancer_hostname,
+    jwt_cert_signing_thumbprint    = azurerm_api_management_certificate.idpay_merchants_token_exchange_cert_jwt.thumbprint
+  })
 
 }
