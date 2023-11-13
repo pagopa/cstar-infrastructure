a542095167ca897c21f67d6f09c2ca04f0f49aeb
petretiandrea <petretiandrea@gmail.com>
2023-11-13 11:27:01 +0100

    added index to onboardingInitiatives

--- src/domains/idpay-common/03_database.tf
+++ src/domains/idpay-common/03_database.tf
@@ -265,6 +265,10 @@ locals {
         {
           keys   = ["userId"]
           unique = false
+        },
+        {
+          keys   = ["onboardingInitiatives"],
+          unique = false
         }
       ]
     },
