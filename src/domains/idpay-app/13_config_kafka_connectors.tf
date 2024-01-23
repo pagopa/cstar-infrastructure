c0a2b860ce27ddabd4de255bd7f308accd80d064
stefanodel <stefano11.delia@nttdata.com>
2024-01-23 16:00:08 +0100

    [IDP-2270] Added tf config kafka connector

---
+++ src/domains/idpay-app/13_config_kafka_connectors.tf
@@ -0,0 +1,34 @@
+resource "null_resource" "cosmos_connector" {
+  #TODO: DA VEDERE COME SCATENARE IL TRIGGER
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      curl -X PUT {host}/connectors/cosmos-connector/config \
+      -H "Content-Type: application/json" \
+      --data '{
+        "connector.class": "com.mongodb.kafka.connect.MongoSourceConnector",
+        "connection.uri": "${var.COSMOS_CONNECTION_STRING}",
+        "database": "<db>",
+        "collection": "<collection>",
+        "topic.namespace.map": "{\"<db>.<collection>\": \"<topic_you_want>\"}",
+        "server.api.version": "4.2",
+        "copy.existing": false,
+        "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+        "key.converter.schemas.enable": false,
+        "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+        "value.converter.schemas.enable": false,
+        "publish.full.document.only": true,
+        "output.json.formatter": "com.mongodb.kafka.connect.source.json.formatter.SimplifiedJson",
+        "heartbeat.interval.ms": 5000,
+        "mongo.errors.log.enable": true,
+        "output.format.value": "json",
+        "output.format.key": "schema",
+        "output.schema.key": "{\"type\":\"record\",\"name\":\"keySchema\",\"fields\":[{\"name\":\"fullDocument.stream_id\",\"type\":\"string\"}]}",
+        "pipeline": "[{\"$match\":{\"operationType\":{\"$in\":[\"insert\",\"update\",\"replace\"]}}},{\"$project\":{\"_id\":1,\"fullDocument\":1,\"ns\":1,\"documentKey\":1}},{\"$match\":{\"fullDocument.status\":{\"$in\":[\"AUTHORIZED\",\"CANCELED\",\"REWARDED\"]}}}]"
+      }'
+    EOT
+  }
+}
