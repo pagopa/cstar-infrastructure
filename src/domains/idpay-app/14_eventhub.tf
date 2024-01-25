449c1c3569e4f12ae4a2af08924cf3d6d30d157d
GiovanaSolorzano <giovanaelvira.solorzanochirieleison@nttdata.com>
2024-01-25 17:23:41 +0100

    IDP-2274 update counts

--- src/domains/idpay-app/14_eventhub.tf
+++ src/domains/idpay-app/14_eventhub.tf
@@ -1,6 +1,5 @@
 locals {
-  enable_evh = 0 //var.env_short == "d" ? 1 : 0
-  enable_secret = 0
+  enable_evh = 1 //var.env_short == "d" ? 1 : 0
   mongodb_connection_uri_template = "mongodb://%s:%s@%s.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@%s@"
   jaas_config_template_idpay = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$ConnectionString\" password=\"%s\";"
 }
@@ -32,7 +31,7 @@ resource "azurerm_eventhub_namespace_authorization_rule" "evh_namespace_access_k
 
 resource "azurerm_key_vault_secret" "event_hub_root_key_idpay_00" {
   name         = "evh-root-jaas-config-idpay-00"
-  value        = format(local.jaas_config_template_idpay, azurerm_eventhub_namespace_authorization_rule.evh_namespace_access_key[count.index].primary_connection_string)
+  value        = format(local.jaas_config_template_idpay, azurerm_eventhub_namespace_authorization_rule.evh_namespace_access_key[0].primary_connection_string)
   content_type = "text/plain"
 
   key_vault_id = data.azurerm_key_vault.kv.id
