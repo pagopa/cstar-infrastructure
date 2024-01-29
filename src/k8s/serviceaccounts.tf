5fe1373e9cdb37e74941632d3d0581b9940e6752
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-01-29 16:28:35 +0100

    restored k8s folder

--- src/k8s/serviceaccounts.tf
+++ src/k8s/serviceaccounts.tf
@@ -1,8 +1,9 @@
-
-module "kubernetes_service_account" {
-  source    = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_service_account?ref=v7.50.0"
-  name      = "azure-devops"
-  namespace = "kube-system"
+resource "kubernetes_service_account" "azure_devops" {
+  metadata {
+    name      = "azure-devops"
+    namespace = "kube-system"
+  }
+  automount_service_account_token = false
 }
 
 
@@ -49,12 +50,22 @@ resource "kubernetes_role_binding" "deployer_binding" {
   }
 }
 
+data "kubernetes_secret" "azure_devops_secret" {
+  metadata {
+    name      = kubernetes_service_account.azure_devops.default_secret_name
+    namespace = "kube-system"
+  }
+  binary_data = {
+    "ca.crt" = ""
+    "token"  = ""
+  }
+}
 
 #tfsec:ignore:AZU023
 resource "azurerm_key_vault_secret" "azure_devops_sa_token" {
-  depends_on   = [module.kubernetes_service_account]
+  depends_on   = [kubernetes_service_account.azure_devops]
   name         = "aks-azure-devops-sa-token"
-  value        = module.kubernetes_service_account.sa_token
+  value        = data.kubernetes_secret.azure_devops_secret.binary_data["token"] # base64 value
   content_type = "text/plain"
 
   key_vault_id = local.key_vault_id
@@ -62,9 +73,9 @@ resource "azurerm_key_vault_secret" "azure_devops_sa_token" {
 
 #tfsec:ignore:AZU023
 resource "azurerm_key_vault_secret" "azure_devops_sa_cacrt" {
-  depends_on   = [module.kubernetes_service_account]
+  depends_on   = [kubernetes_service_account.azure_devops]
   name         = "aks-azure-devops-sa-cacrt"
-  value        = module.kubernetes_service_account.sa_ca_cert
+  value        = data.kubernetes_secret.azure_devops_secret.binary_data["ca.crt"] # base64 value
   content_type = "text/plain"
 
   key_vault_id = local.key_vault_id
