15594d5c3be8dd1fd67c7ccb636f04bc6e7de873
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-01-29 16:21:10 +0100

    update service account module

--- src/k8s/serviceaccounts.tf
+++ src/k8s/serviceaccounts.tf
@@ -1,6 +1,6 @@
 
 module "kubernetes_service_account" {
-  source    = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_service_account?ref=19dcf4b670fecda555b4f6f7155fc165e84dddf9"
+  source    = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_service_account?ref=v7.50.0"
   name      = "azure-devops"
   namespace = "kube-system"
 }
