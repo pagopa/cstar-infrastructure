f04870bcb7e514736a158e3da038889ecc50cbb5
petretiandrea <petretiandrea@gmail.com>
2024-01-31 12:44:28 +0100

    remove old k8s folder

--- src/k8s/secrets.tf
+++
@@ -1,8 +0,0 @@
-module "key_vault_secrets_query" {
-  source         = "git::https://github.com/pagopa/terraform-azurerm-v3.git//key_vault_secrets_query?ref=v6.15.2"
-  resource_group = local.key_vault_resource_group
-  key_vault_name = local.key_vault_name
-
-  # This list should be specialized for environment
-  secrets = var.secrets_to_be_read_from_kv
-}
