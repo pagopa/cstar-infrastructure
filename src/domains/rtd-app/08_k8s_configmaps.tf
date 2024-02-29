4dfb35033bce08d70b4bb97666f99a9bf63d8a70
TommasoLencioni <tommaso.lencioni@pagopa.it>
2024-02-29 12:19:15 +0100

    feat: [RTD-2501] set default contracts splitting threshold to 5 millions

--- src/domains/rtd-app/08_k8s_configmaps.tf
+++ src/domains/rtd-app/08_k8s_configmaps.tf
@@ -225,7 +225,7 @@ resource "kubernetes_config_map" "rtddecrypter" {
     CSV_TRANSACTION_DECRYPT_HOST       = replace("apim.internal.${var.env}.cstar.pagopa.it", ".prod.", ".")
     SPLITTER_LINE_THRESHOLD            = 2000000,
     AGGREGATES_SPLITTER_LINE_THRESHOLD = 2000000,
-    CONTRACTS_SPLITTER_LINE_THRESHOLD  = 100000,
+    CONTRACTS_SPLITTER_LINE_THRESHOLD  = 5000000,
     ENABLE_CHUNK_UPLOAD                = true,
     CONSUMER_TIMEOUT_MS                = 600000 # 10m
   }, var.configmaps_rtddecrypter)
