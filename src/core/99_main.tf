cbfcb3de37ac12a1caba67e0e8b2f9963384424d
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-05-06 23:03:12 +0200

    updated modules with last parameters

--- src/core/99_main.tf
+++ src/core/99_main.tf
@@ -14,10 +14,12 @@ terraform {
       source  = "hashicorp/random"
       version = "<= 3.6.0"
     }
+
     local = {
       source  = "hashicorp/local"
       version = "<= 2.3.0"
     }
+
     null = {
       source  = "hashicorp/null"
       version = "<= 3.2.1"
