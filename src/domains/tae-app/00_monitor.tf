412bb70f75dc48840ea84211ce7842cd78b48884
Luca Consalvi <luca.consalvi@pagopa.it>
2024-06-28 11:20:17 +0200

    [TAE-223] add alert for fail delete local blob

--- src/domains/tae-app/00_monitor.tf
+++ src/domains/tae-app/00_monitor.tf
@@ -1594,3 +1594,54 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "at-least-one-pending-
   }
 
 }
+
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "fail_to_delete_blob" {
+
+  count = var.env_short == "p" ? 1 : 0
+
+  name                = "cstar-${var.env_short}-decrypter-fail-to-delete-blob"
+  resource_group_name = data.azurerm_resource_group.monitor_rg.name
+  location            = data.azurerm_resource_group.monitor_rg.location
+
+  evaluation_frequency = "P1D"
+  window_duration      = "P1D"
+  scopes               = [data.azurerm_log_analytics_workspace.log_analytics.id]
+  severity             = 2
+  criteria {
+    query                   = <<-QUERY
+      AppTraces
+      | where AppRoleName == "rtddecrypter"
+      | where SeverityLevel == 2
+      | where Message startswith "Failed to delete local blob file:"
+      QUERY
+    time_aggregation_method = "Count"
+    threshold               = 0
+    operator                = "GreaterThan"
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
+  }
+
+  auto_mitigation_enabled          = false
+  workspace_alerts_storage_enabled = false
+  description                      = "Triggers whenever at least one local blob file was not deleted."
+  display_name                     = "cstar-${var.env_short}-decrypter-fail-to-delete-blob"
+  enabled                          = true
+
+  skip_query_validation = false
+  action {
+    action_groups = [
+      azurerm_monitor_action_group.send_to_operations
+    ]
+    custom_properties = {
+      key  = "value"
+      key2 = "value2"
+    }
+  }
+
+  tags = {
+    key = "Sender Monitoring"
+  }
+}
