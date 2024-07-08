662ff66edca470a7c2e8ac2ed980f0c673cd384a
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-07-08 14:54:37 +0200

    fix ingress endpoint to mil.weu

--- src/domains/mil-app-poc/00_network.tf
+++ src/domains/mil-app-poc/00_network.tf
@@ -2,3 +2,8 @@ data "azurerm_dns_zone" "public" {
   name                = var.env_short == "p" ? "${var.dns_zone_prefix}.${var.external_domain}" : "${var.env}.${var.dns_zone_prefix}.${var.external_domain}"
   resource_group_name = local.vnet_core_resource_group_name
 }
+
+data "azurerm_private_dns_zone" "internal" {
+  name                = local.internal_dns_zone_name
+  resource_group_name = local.internal_dns_zone_resource_group_name
+}
