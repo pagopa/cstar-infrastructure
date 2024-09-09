e373fd1daf22465beb85b231d071fb9ea744df94
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-09-09 11:50:42 +0200

    revert remove of remote state

--- src/domains/idpay-common/99_main.tf
+++ src/domains/idpay-common/99_main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.40.0"
+      version = "~> 3.53"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "= 2.21.0"
+      version = "<= 2.50.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "= 3.1.1"
+      version = "<= 3.2.1"
     }
     pkcs12 = {
       source  = "chilicat/pkcs12"
@@ -32,6 +32,17 @@ provider "azurerm" {
 data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "current" {}
+
+data "terraform_remote_state" "core" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = var.terraform_remote_state_core.resource_group_name
+    storage_account_name = var.terraform_remote_state_core.storage_account_name
+    container_name       = var.terraform_remote_state_core.container_name
+    key                  = var.terraform_remote_state_core.key
+  }
+}
 
 data "azurerm_dns_zone" "public" {
   name                = join(".", [var.dns_zone_prefix, var.external_domain])
