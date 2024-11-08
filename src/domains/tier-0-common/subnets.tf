b1823bea4c404bedf3696b1547b6ff2143a2b779
antoniotarricone <antonio.tarricone@pagopa.it>
2024-11-08 14:49:38 +0100

    Subnet for private endpoint is now imported.

--- src/domains/tier-0-common/subnets.tf
+++ src/domains/tier-0-common/subnets.tf
@@ -11,3 +11,17 @@ resource "azurerm_subnet" "aca" {
   virtual_network_name = data.azurerm_virtual_network.intern.name
   address_prefixes     = [var.aca_snet_cidr]
 }
+
+# ------------------------------------------------------------------------------
+# Private endpoints subnet.
+# ------------------------------------------------------------------------------
+variable "core_private_endpoints_subnet_name" {
+  type    = string
+  default = "private-endpoint-snet"
+}
+
+data "azurerm_subnet" "private_endpoints" {
+  name                 = var.core_private_endpoints_subnet_name
+  virtual_network_name = var.core_virtual_network_name
+  resource_group_name  = var.core_virtual_network_resource_group_name
+}
