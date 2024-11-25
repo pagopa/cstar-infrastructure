80ef9581f9b0997af72bf9aaa55434ebfab2c351
petretiandrea <petretiandrea@gmail.com>
2024-11-25 11:08:14 +0100

    fix typo

--- src/domains/rtp-common/appinsights.tf
+++ src/domains/rtp-common/appinsights.tf
@@ -1,11 +1,11 @@
 
 data "azurerm_application_insights" "appinsights" {
-  name = "${var.prefix}-${var.env_short}-appinsights"
+  name                = "${var.prefix}-${var.env_short}-appinsights"
   resource_group_name = "${var.prefix}-${var.env_short}-monitor-rg"
 }
 
 # ------------------------------------------------------------------------------
-# Storing CosmosDB endpoint in the general key vault.
+# Storing AppInsights endpoint in the rtp key vault.
 # ------------------------------------------------------------------------------
 resource "azurerm_key_vault_secret" "appinisights_connection_string_kv" {
   name         = "appinsights-connection-string"
