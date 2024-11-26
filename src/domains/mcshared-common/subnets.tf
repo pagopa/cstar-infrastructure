ab818121ac23149e6c85053bf2b3b631f02626e4
antoniotarricone <antonio.tarricone@pagopa.it>
2024-11-26 17:53:53 +0100

    Providers upgraded.

--- src/domains/mcshared-common/subnets.tf
+++ src/domains/mcshared-common/subnets.tf
@@ -6,10 +6,11 @@ variable "aca_snet_cidr" {
 }
 
 resource "azurerm_subnet" "aca" {
-  name                 = "${local.project}-aca-snet"
-  resource_group_name  = data.azurerm_virtual_network.intern.resource_group_name
-  virtual_network_name = data.azurerm_virtual_network.intern.name
-  address_prefixes     = [var.aca_snet_cidr]
+  name                              = "${local.project}-aca-snet"
+  resource_group_name               = data.azurerm_virtual_network.intern.resource_group_name
+  virtual_network_name              = data.azurerm_virtual_network.intern.name
+  address_prefixes                  = [var.aca_snet_cidr]
+  private_endpoint_network_policies = "Enabled"
 }
 
 # ------------------------------------------------------------------------------
