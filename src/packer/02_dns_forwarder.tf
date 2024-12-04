c11d4e991894154de1215fbd575a20b5e911e6dd
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-12-04 14:48:27 +0100

    pre-commit fixs

--- src/packer/02_dns_forwarder.tf
+++ src/packer/02_dns_forwarder.tf
@@ -1,5 +1,5 @@
 module "dns_forwarder_image" {
-  source = "./.terraform/modules/__v3__/dns_forwarder_vm_image"
+  source              = "./.terraform/modules/__v3__/dns_forwarder_vm_image"
   resource_group_name = data.azurerm_resource_group.vnet_rg.name
   location            = var.location
   image_name          = "${local.project}-dns-forwarder-ubuntu2204-image"
