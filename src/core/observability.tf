c6606a9c9b17128eda3d2fe24b40f735d698c8e3
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2025-01-14 12:35:56 +0100

    log-analytics: add ignore changes sku

--- src/core/observability.tf
+++ src/core/observability.tf
@@ -14,7 +14,7 @@ data "azurerm_log_analytics_workspace" "default" {
 }
 
 resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
-  name                = format("%s-law", local.project)
+  name                = "${local.project}-law"
   location            = azurerm_resource_group.monitor_rg.location
   resource_group_name = azurerm_resource_group.monitor_rg.name
   sku                 = var.law_sku
@@ -22,6 +22,12 @@ resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
   daily_quota_gb      = var.law_daily_quota_gb
 
   tags = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      sku
+    ]
+  }
 }
 
 # Application insights
