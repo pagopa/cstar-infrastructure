5c4e8dc56e91ba64536d15485b32feb318a55efa
Michele Fucito <michele.fucito@digitouch.it>
2025-01-21 14:14:09 +0100

    chore: [TAE-219] add alert for decrypter failed blob splitting

--- src/domains/tae-app/00_monitor.tf
+++ src/domains/tae-app/00_monitor.tf
@@ -771,6 +771,58 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "wrong_name_format" {
   }
 }
 
+resource "azurerm_monitor_scheduled_query_rules_alert_v2" "decrypter_failed_splitting_blob" {
+
+  count = var.env_short == "p" ? 1 : 0
+
+  name                = "cstar-${var.env_short}-decrypter-failed-splitting-blob"
+  resource_group_name = data.azurerm_resource_group.monitor_rg.name
+  location            = data.azurerm_resource_group.monitor_rg.location
+
+  evaluation_frequency = "PT5M"
+  window_duration      = "PT5M"
+  scopes               = [data.azurerm_log_analytics_workspace.log_analytics.id]
+  severity             = 1
+  criteria {
+    query                   = <<-QUERY
+      AppTraces
+      | where AppRoleName == "rtddecrypter"
+      | where SeverityLevel == 3
+      | where Message startswith "Failed splitting blob"
+      QUERY
+    time_aggregation_method = "Count"
+    threshold               = 0
+    operator                = "GreaterThan"
+
+    failing_periods {
+      minimum_failing_periods_to_trigger_alert = 1
+      number_of_evaluation_periods             = 1
+    }
+  }
+
+  auto_mitigation_enabled          = false
+  workspace_alerts_storage_enabled = false
+  description                      = "Triggers an alert when a blob is not correctly split during processing."
+  display_name                     = "cstar-${var.env_short}-decrypter-wrong-name-format-#ACQ"
+  enabled                          = true
+
+  skip_query_validation = false
+  action {
+    action_groups = [
+      azurerm_monitor_action_group.send_to_operations[0].id,
+      azurerm_monitor_action_group.send_to_zendesk[0].id
+    ]
+    custom_properties = {
+      key  = "value"
+      key2 = "value2"
+    }
+  }
+
+  tags = {
+    key = "Sender Monitoring"
+  }
+}
+
 resource "azurerm_monitor_scheduled_query_rules_alert_v2" "ack_ingestor_failures" {
 
   count = var.env_short == "p" ? 1 : 0
