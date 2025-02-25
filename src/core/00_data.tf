b922cd83d6d0a41ee63a21cbc3b4d7774bf0ae03
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2025-02-25 11:39:22 +0100

    feat: Container app added privatelink dns (#1388)
    * added container app dns private link
    
    * pre-commit fixs

---
+++ src/core/00_data.tf
@@ -0,0 +1,13 @@
+data "azurerm_virtual_network" "securehub_hub" {
+  name                = local.vnet_securehub_core_hub_name
+  resource_group_name = local.vnet_securehub_rg_name
+}
+
+data "azurerm_virtual_network" "securehub_platform" {
+  name                = local.vnet_securehub_spoke_platform_name
+  resource_group_name = local.vnet_securehub_rg_name
+}
+
+data "azurerm_resources" "vnets" {
+  type = "Microsoft.Network/virtualNetworks"
+}
