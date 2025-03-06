14bcd0d784db2b2479d4e6bbf40e8cbc69a9e1a0
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2025-03-06 09:42:39 +0100

    feat: SRTP-274 mcshared cae with nat gw and private endpoint (#1401)
    * added subnet for cae with workload profile
    
    * added subnet for cae with workload profile
    
    * added subnet for cae with workload profile
    
    * minor fix
    
    * container app changed subnet
    
    * added privete endpoint for container app
    
    * upgrated provider azurerm
    
    * container app added locks and fix configuration
    
    * fix natgateway sunet
    
    * core: changed private dns zone for container app
    
    * private dns zone container app now is linked with is own private dns
    
    * fix: update service URL for authorization microservice to use container app environment domain
    
    * pre-commit fixs

--- src/domains/mcshared-common/container_app_environment.tf
+++ src/domains/mcshared-common/container_app_environment.tf
@@ -2,22 +2,57 @@
 # ACA Environment.
 # ------------------------------------------------------------------------------
 resource "azurerm_container_app_environment" "mcshared" {
-  name                           = "${local.project}-cae"
-  location                       = azurerm_resource_group.app.location
-  resource_group_name            = azurerm_resource_group.app.name
+  name                = "${local.project}-cae"
+  location            = azurerm_resource_group.app.location
+  resource_group_name = azurerm_resource_group.app.name
+
   log_analytics_workspace_id     = data.azurerm_log_analytics_workspace.core.id
+  infrastructure_subnet_id       = azurerm_subnet.subnet_mcshared_cae.id
   internal_load_balancer_enabled = true
-  infrastructure_subnet_id       = azurerm_subnet.aca.id
-  tags                           = local.tags
   zone_redundancy_enabled        = var.aca_env_zones_enabled
+
   workload_profile {
     name                  = "Consumption"
     workload_profile_type = "Consumption"
     minimum_count         = 0
-    maximum_count         = 10
+    maximum_count         = 0
   }
+
+  tags = local.tags
 
   depends_on = [
     azurerm_subnet.aca
   ]
+
+  lifecycle {
+    ignore_changes = [
+      infrastructure_resource_group_name
+    ]
+  }
+}
+
+resource "azurerm_management_lock" "mcshared_cae_lock" {
+  name       = "${local.project}-cae-lock"
+  scope      = azurerm_container_app_environment.mcshared.id
+  lock_level = "CanNotDelete"
+  notes      = "This Container App Environment should not be deleted"
+}
+
+resource "azurerm_private_endpoint" "private_endpoint_mcshared_cae" {
+  name                = azurerm_container_app_environment.mcshared.name
+  location            = azurerm_resource_group.app.location
+  resource_group_name = azurerm_resource_group.app.name
+  subnet_id           = data.azurerm_subnet.private_endpoints.id
+
+  private_service_connection {
+    name                           = azurerm_container_app_environment.mcshared.name
+    private_connection_resource_id = azurerm_container_app_environment.mcshared.id
+    is_manual_connection           = false
+    subresource_names              = ["managedEnvironments"]
+  }
+
+  private_dns_zone_group {
+    name                 = "${local.project}-cae"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.container_app.id]
+  }
 }
