d0a3ce13b7699247504d8332adaf231e23131722
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-03-11 15:42:05 +0100

    feat: Implement Redis resource group and module configuration with private endpoint

--- src/domains/mil-common-poc/03_redis.tf
+++ src/domains/mil-common-poc/03_redis.tf
@@ -1,22 +1,49 @@
-data "azurerm_redis_cache" "idpay_redis" {
-  name                = local.redis_idpay_name
-  resource_group_name = local.redis_idpay_resource_group_name
+resource "azurerm_resource_group" "redis_rg" {
+  name     = "${local.project}-redis-rg"
+  location = var.location
+
+  tags = var.tags
 }
 
-resource "azurerm_key_vault_secret" "emd_redis_primary_connection_hostname" {
+module "mil_redis" {
+  source                        = "./.terraform/modules/__v3__/redis_cache"
+  name                          = "${local.project}-redis"
+  location                      = azurerm_resource_group.redis_rg.location
+  resource_group_name           = azurerm_resource_group.redis_rg.name
+  capacity                      = var.redis_capacity
+  enable_non_ssl_port           = false
+  family                        = var.redis_family
+  sku_name                      = var.redis_sku_name
+  enable_authentication         = true
+  # subnet_id                     = module.redis_mil_snet.id
+  redis_version                 = "6"
+  public_network_access_enabled = false
 
-  name         = "emd-redis-primary-connection-hostname"
-  value        = data.azurerm_redis_cache.idpay_redis.hostname
-  content_type = "text/plain"
+  private_endpoint = {
+    enabled              = true
+    virtual_network_id   = data.azurerm_virtual_network.vnet_weu.id
+    subnet_id            = module.redis_mil_snet.id
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.privatelink_redis.id]
+  }
 
-  key_vault_id = data.azurerm_key_vault.kv_domain.id
+  tags  = var.tags
+  zones = [1, 2, 3]
 }
 
-resource "azurerm_key_vault_secret" "emd_redis_primary_access_key" {
-
-  name         = "emd-redis-primary-access-key"
-  value        = data.azurerm_redis_cache.idpay_redis.primary_access_key
-  content_type = "text/plain"
-
-  key_vault_id = data.azurerm_key_vault.kv_domain.id
-}
+# resource "azurerm_key_vault_secret" "emd_redis_primary_connection_hostname" {
+#
+#   name         = "emd-redis-primary-connection-hostname"
+#   value        = data.azurerm_redis_cache.idpay_redis.hostname
+#   content_type = "text/plain"
+#
+#   key_vault_id = data.azurerm_key_vault.kv_domain.id
+# }
+#
+# resource "azurerm_key_vault_secret" "emd_redis_primary_access_key" {
+#
+#   name         = "emd-redis-primary-access-key"
+#   value        = data.azurerm_redis_cache.idpay_redis.primary_access_key
+#   content_type = "text/plain"
+#
+#   key_vault_id = data.azurerm_key_vault.kv_domain.id
+# }
