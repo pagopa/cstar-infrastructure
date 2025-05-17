d60ab5156a0e1b1c6c15d4f98d42924d056425af
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-05-18 00:06:57 +0200

    was removed azdo resources

--- src/core/azure_devops_agent.tf
+++ src/core/azure_devops_agent.tf
@@ -1,52 +1,52 @@
-resource "azurerm_resource_group" "azdo_rg" {
-  count    = var.enable_azdoa ? 1 : 0
-  name     = "${local.project}-azdoa-rg"
-  location = var.location
-
-  tags = var.tags
-}
-
-module "azdoa_vmss_ubuntu_app" {
-  source = "./.terraform/modules/__v3__/azure_devops_agent"
-  count  = var.enable_azdoa ? 1 : 0
-
-  name                = local.azuredevops_agent_vm_app_name
-  resource_group_name = azurerm_resource_group.azdo_rg[0].name
-  subnet_id           = module.azdoa_snet[0].id
-  subscription_id     = data.azurerm_subscription.current.subscription_id
-  location            = var.location
-  source_image_name   = var.azdoa_image_name
-  vm_sku              = var.azdoa_agent_app_vm_sku
-
-  tags = var.tags
-}
-
-module "azdoa_vmss_ubuntu_infra" {
-  source = "./.terraform/modules/__v3__/azure_devops_agent"
-  count  = var.enable_azdoa ? 1 : 0
-
-  name                = local.azuredevops_agent_vm_infra_name
-  resource_group_name = azurerm_resource_group.azdo_rg[0].name
-  subnet_id           = module.azdoa_snet[0].id
-  subscription_id     = data.azurerm_subscription.current.subscription_id
-  location            = var.location
-  source_image_name   = var.azdoa_image_name
-  vm_sku              = var.azdoa_agent_infra_vm_sku
-
-  tags = var.tags
-}
-
-module "azdoa_agent_vmss_ubuntu_perf" {
-  source = "./.terraform/modules/__v3__/azure_devops_agent"
-  count  = var.enable_azdoa_agent_performance ? 1 : 0
-
-  name                = local.azuredevops_agent_vm_perf_name
-  resource_group_name = azurerm_resource_group.azdo_rg[0].name
-  subnet_id           = module.azdoa_snet[0].id
-  subscription_id     = data.azurerm_subscription.current.subscription_id
-  location            = var.location
-  source_image_name   = var.azdoa_image_name
-  vm_sku              = var.azdoa_agent_performance_vm_sku
-
-  tags = var.tags
-}
+# resource "azurerm_resource_group" "azdo_rg" {
+#   count    = var.enable_azdoa ? 1 : 0
+#   name     = "${local.project}-azdoa-rg"
+#   location = var.location
+#
+#   tags = var.tags
+# }
+#
+# module "azdoa_vmss_ubuntu_app" {
+#   source = "./.terraform/modules/__v3__/azure_devops_agent"
+#   count  = var.enable_azdoa ? 1 : 0
+#
+#   name                = local.azuredevops_agent_vm_app_name
+#   resource_group_name = azurerm_resource_group.azdo_rg[0].name
+#   subnet_id           = module.azdoa_snet[0].id
+#   subscription_id     = data.azurerm_subscription.current.subscription_id
+#   location            = var.location
+#   source_image_name   = var.azdoa_image_name
+#   vm_sku              = var.azdoa_agent_app_vm_sku
+#
+#   tags = var.tags
+# }
+#
+# module "azdoa_vmss_ubuntu_infra" {
+#   source = "./.terraform/modules/__v3__/azure_devops_agent"
+#   count  = var.enable_azdoa ? 1 : 0
+#
+#   name                = local.azuredevops_agent_vm_infra_name
+#   resource_group_name = azurerm_resource_group.azdo_rg[0].name
+#   subnet_id           = module.azdoa_snet[0].id
+#   subscription_id     = data.azurerm_subscription.current.subscription_id
+#   location            = var.location
+#   source_image_name   = var.azdoa_image_name
+#   vm_sku              = var.azdoa_agent_infra_vm_sku
+#
+#   tags = var.tags
+# }
+#
+# module "azdoa_agent_vmss_ubuntu_perf" {
+#   source = "./.terraform/modules/__v3__/azure_devops_agent"
+#   count  = var.enable_azdoa_agent_performance ? 1 : 0
+#
+#   name                = local.azuredevops_agent_vm_perf_name
+#   resource_group_name = azurerm_resource_group.azdo_rg[0].name
+#   subnet_id           = module.azdoa_snet[0].id
+#   subscription_id     = data.azurerm_subscription.current.subscription_id
+#   location            = var.location
+#   source_image_name   = var.azdoa_image_name
+#   vm_sku              = var.azdoa_agent_performance_vm_sku
+#
+#   tags = var.tags
+# }
