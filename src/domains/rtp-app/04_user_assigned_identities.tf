a3fbeb4a0b771d2490d929e47b731afea7eb9d38
Luca Consalvi <117908483+lucaconsalvi@users.noreply.github.com>
2025-05-27 10:26:36 +0200

    chore: Add rtp sender resources. (#1435)
    * Add rtp sender resources.
    
    * Refactor certificate management for App Gateway module
    
    Centralized trusted client certificate handling using locals to simplify and improve maintainability. Replaced hardcoded certificate lists with dynamic lists based on environment to reduce redundancy and streamline updates.
    
    ---------
    
    Co-authored-by: Fabio Felici <fabio.felici@pagopa.it>

--- src/domains/rtp-app/04_user_assigned_identities.tf
+++ src/domains/rtp-app/04_user_assigned_identities.tf
@@ -17,3 +17,13 @@ resource "azurerm_user_assigned_identity" "activator" {
   name                = "${local.project}-activator-id"
   tags                = var.tags
 }
+
+# ------------------------------------------------------------------------------
+# Identity for rtp-sender microservice.
+# ------------------------------------------------------------------------------
+resource "azurerm_user_assigned_identity" "sender" {
+  resource_group_name = azurerm_resource_group.managed_identities_rg.name
+  location            = azurerm_resource_group.managed_identities_rg.location
+  name                = "${local.project}-sender-id"
+  tags                = var.tags
+}
