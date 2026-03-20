f3adf83e153f281349a8acef7c42f8a54e8c44bc
Umberto Coppola Bottazzi <umberto.coppolabottazzi@pagopa.it>
2026-03-20 17:01:49 +0100

    feat: [MDC] Remove mdc WEU (#1486)
    * mdc remove api@uat
    
    * remove cosmos db from tf state
    
    * remove mdc weu

--- src/domains/mil-app-poc/01_keyvault.tf
+++
@@ -1,7 +0,0 @@
-#tfsec:ignore:AZU023
-resource "azurerm_key_vault_secret" "appinsights-instrumentation-key" {
-  key_vault_id = data.azurerm_key_vault.kv_domain.id
-  name         = "appinsights-instrumentation-key"
-  value        = data.azurerm_application_insights.application_insights.connection_string
-  content_type = "text/plain"
-}
